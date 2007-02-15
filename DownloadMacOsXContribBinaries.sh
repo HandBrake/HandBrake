20cea8bdab320018c97c42682981079f157f54bc
prigaux <pri@nopapers.org>
2007-02-15 07:25:45 +0000

    Contrib server change to sr55's one, temporary due to main server problems.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/branches/0.8.0_beta2@337 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- DownloadMacOsXContribBinaries.sh
+++ DownloadMacOsXContribBinaries.sh
@@ -10,7 +10,8 @@ if [ -f contrib/DarwinContribVersion.txt ]; then
   fi
 fi
 
-HOST=download.mediafork.dynalias.com
+#HOST=download.mediafork.dynalias.com
+HOST=sr55.ashosted.com
 FILE=contribbin-darwin-$VERSION.tar.gz
 URL=http://$HOST/contrib/$FILE
 
