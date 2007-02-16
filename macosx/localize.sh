cbd1e2cd35b1241c7a71441f7e5e3f3b39b81a8f
prigaux <pri@nopapers.org>
2007-02-16 08:06:22 +0000

    Makefile modifications, the MF_BUILD and MF_VERSION are taken from Makefile.config. the Info.plist is generated from the localize.sh script in macosx folder. Xcode settings are set at build time, no need the play around.
    If modifications are needed in Info.plist, you must correct the localize script to show the right output.
    
    The info.plist informations are seen in aboutbox of Mac GUI.
    
    git-svn-id: svn://svn.handbrake.fr/HandBrake/branches/0.8.0_beta2_5.1@364 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- macosx/localize.sh
+++ macosx/localize.sh
@@ -8,3 +8,43 @@ do
 	cp -r English.lproj $l.lproj
 	cp ../../../macosx/i18n/$l.strings $l.lproj/Localizable.strings
 done
+
+echo Generating Info.plist with correct version information
+cd ..
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+<plist version=\"1.0\">
+<dict>
+        <key>CFBundleDevelopmentRegion</key>
+        <string>English</string>
+        <key>CFBundleDisplayName</key>
+        <string>MediaFork</string>
+        <key>CFBundleExecutable</key>
+        <string>MediaFork</string>
+        <key>CFBundleGetInfoString</key>
+        <string>0.8.0b1</string>
+        <key>CFBundleIconFile</key>
+        <string>MediaFork</string>
+        <key>CFBundleIdentifier</key>
+        <string>org.mediafork.dynalias</string>
+        <key>CFBundleInfoDictionaryVersion</key>
+        <string>6.0</string>
+        <key>CFBundleName</key>
+        <string>MediaFork</string>
+        <key>CFBundlePackageType</key>
+        <string>APPL</string>
+        <key>CFBundleShortVersionString</key>
+        <string>$2</string>
+        <key>CFBundleSignature</key>
+        <string>HB##</string>
+        <key>CFBundleVersion</key>
+        <string>$3</string>
+        <key>NSHumanReadableCopyright</key>
+        <string>MediaFork Devs</string>
+        <key>NSMainNibFile</key>
+        <string>MainMenu</string>
+        <key>NSPrincipalClass</key>
+        <string>NSApplication</string>
+</dict>
+</plist>" > Info.plist
+
