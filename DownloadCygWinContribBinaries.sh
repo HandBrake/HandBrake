d609c755e48fb040f5e3f5538fb408cf95bb7eb0
sr55 <sr55.hb@outlook.com>
2007-04-05 16:46:25 +0000

    Uploaded new contrib packaged which is compiled under GCC4 - Fixes many stack dump issues under CYGWIN
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@478 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- DownloadCygWinContribBinaries.sh
+++ DownloadCygWinContribBinaries.sh
@@ -1,18 +1,12 @@
 #! /bin/sh
 
-
 HOST=http://download.m0k.org
-FILE=contribbin-cygwin-0001.tar.gz
+FILE=contribbin-cygwin-0002.tar.gz
 URL=$HOST/handbrake/contrib/$FILE
-# Check for internet connectivity
-#if ! host $HOST > /dev/null 2>&1; then
-#  echo "Please connect to the Internet (could not resolve $HOST)."
-#  exit 1
-#fi
 
 # Get and install the package
 echo "Getting contribs ($VERSION)..."
-( cd contrib && rm -f contribbin-cygwin-0001.tar.gz && wget $URL && rm -Rf lib include && tar xzf contribbin-cygwin-0001.tar.gz && ranlib lib/*.a ) || exit 1
+( cd contrib && rm -f contribbin-cygwin-0002.tar.gz && wget $URL && rm -Rf lib include && tar xzf contribbin-cygwin-0002.tar.gz && ranlib lib/*.a ) || exit 1
 
 exit 0
 
