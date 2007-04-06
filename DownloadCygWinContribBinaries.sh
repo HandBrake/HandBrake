889281d35dcd4358897e821c01080edfc9cc555f
sr55 <sr55.hb@outlook.com>
2007-04-06 13:52:47 +0000

    Pre-compiled version of a52dec updated with maurj's patch included.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@485 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- DownloadCygWinContribBinaries.sh
+++ DownloadCygWinContribBinaries.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 HOST=http://download.m0k.org
-FILE=contribbin-cygwin-0002.tar.gz
+FILE=contribbin-cygwin-0003.tar.gz
 URL=$HOST/handbrake/contrib/$FILE
 
 # Get and install the package
 echo "Getting contribs ($VERSION)..."
-( cd contrib && rm -f contribbin-cygwin-0002.tar.gz && wget $URL && rm -Rf lib include && tar xzf contribbin-cygwin-0002.tar.gz && ranlib lib/*.a ) || exit 1
+( cd contrib && rm -f contribbin-cygwin-0003.tar.gz && wget $URL && rm -Rf lib include && tar xzf contribbin-cygwin-0003.tar.gz && ranlib lib/*.a ) || exit 1
 
 exit 0
 
