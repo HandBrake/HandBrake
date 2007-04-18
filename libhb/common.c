42605214645c734218a013140e1bfa8bcecd8baa
dynaflash <dynaflashtech@gmail.com>
2007-04-18 19:15:02 +0000

    Add 384 kbps to Audio Bitrates selection for use with 6 channel discrete - We still need to filter out which bitrates are available depending on mixdown. Right now, you can select 320 in gui for a stereo track even though libhb limits it to 160, so its misleading.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@530 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- libhb/common.c
+++ libhb/common.c
@@ -31,7 +31,7 @@ hb_rate_t hb_audio_bitrates[] =
 { {  "32",  32 }, {  "40",  40 }, {  "48",  48 }, {  "56",  56 },
   {  "64",  64 }, {  "80",  80 }, {  "96",  96 }, { "112", 112 },
   { "128", 128 }, { "160", 160 }, { "192", 192 }, { "224", 224 },
-  { "256", 256 }, { "320", 320 } };
+  { "256", 256 }, { "320", 320 }, { "384", 384 } };
 int hb_audio_bitrates_count = sizeof( hb_audio_bitrates ) /
                               sizeof( hb_rate_t );
 int hb_audio_bitrates_default = 8; /* 128 kbps */
