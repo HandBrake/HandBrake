62fc0cab70345140dea39289922e97721fc48fff
sr55 <sr55.hb@outlook.com>
2007-06-13 18:00:56 +0000

    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@616 b64f7644-9d1e-0410-96f1-a4d463321fa5

---
+++ win/Handbrake/Settings.vb
@@ -0,0 +1,11 @@
+
+Namespace My
+    
+    'This class allows you to handle specific events on the settings class:
+    ' The SettingChanging event is raised before a setting's value is changed.
+    ' The PropertyChanged event is raised after a setting's value is changed.
+    ' The SettingsLoaded event is raised after the setting values are loaded.
+    ' The SettingsSaving event is raised before the setting values are saved.
+    Partial Friend NotInheritable Class MySettings
+    End Class
+End Namespace
