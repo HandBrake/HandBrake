7401a94cb655fe576dbca898b42bcd265463db7d
sr55 <sr55.hb@outlook.com>
2007-09-22 15:24:17 +0000

    Cygwin Contrib Pack updated to 0012. Also fixes issue with libdts shortcut instead of proper library (libdts is libdca file)
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@978 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- DownloadCygWinContribBinaries.sh
+++ DownloadCygWinContribBinaries.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 HOST=http://download.m0k.org
-FILE=contribbin-cygwin-0011.tar.gz
+FILE=contribbin-cygwin-0012.tar.gz
 URL=$HOST/handbrake/contrib/$FILE
 
 # Get and install the package
 echo "Getting contribs ($VERSION)..."
-( cd contrib && rm -f contribbin-cygwin-0011.tar.gz && wget $URL && rm -Rf lib include && tar xzf contribbin-cygwin-0011.tar.gz && ranlib lib/*.a ) || exit 1
+( cd contrib && rm -f contribbin-cygwin-0012.tar.gz && wget $URL && rm -Rf lib include && tar xzf contribbin-cygwin-0012.tar.gz && ranlib lib/*.a ) || exit 1
 
 exit 0
 
