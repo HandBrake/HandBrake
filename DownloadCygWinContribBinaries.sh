036dae2df642aa78cf880c9b701babe5dbca3b31
sr55 <sr55.hb@outlook.com>
2008-01-18 16:09:31 +0000

    Cygwin Contrib pack updated to 0014
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@1209 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- DownloadCygWinContribBinaries.sh
+++ DownloadCygWinContribBinaries.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 HOST=http://download.m0k.org
-FILE=contribbin-cygwin-0013.tar.gz
+FILE=contribbin-cygwin-0014.tar.gz
 URL=$HOST/handbrake/contrib/$FILE
 
 # Get and install the package
 echo "Getting contribs ($VERSION)..."
-( cd contrib && rm -f contribbin-cygwin-0013.tar.gz && wget $URL && rm -Rf lib include && tar xzf contribbin-cygwin-0013.tar.gz && ranlib lib/*.a ) || exit 1
+( cd contrib && rm -f contribbin-cygwin-0014.tar.gz && wget $URL && rm -Rf lib include && tar xzf contribbin-cygwin-0014.tar.gz && ranlib lib/*.a ) || exit 1
 
 exit 0
 
