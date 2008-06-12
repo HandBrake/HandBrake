01d77f0e0f6a196de290110a14b965588cdc75e2
sr55 <sr55.hb@outlook.com>
2008-06-12 17:02:33 +0000

    WinGui: - Accidentally updated the project file in the last commit, so the following empty file is now required. It will be used at a later date.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@1509 b64f7644-9d1e-0410-96f1-a4d463321fa5

---
+++ win/C#/Functions/Presets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Handbrake.Functions
+{
+    // This file is going to be used for all the preset stuff, when i get around to switching out to something that works a bit better.
+
+    class Presets
+    {
+        public void loadPresetPanel(frmMain mainWindow)
+        {
+        }
+    }
+}
