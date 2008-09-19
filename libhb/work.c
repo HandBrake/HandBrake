c451bb0b078b4d1bfcea1d9c7ceced9166e15711
jbrjake <jb.rubin@gmail.com>
2008-09-19 14:02:13 +0000

    Spit out the average encoding speed in the verbose log at the end of a job.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@1727 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- libhb/work.c
+++ libhb/work.c
@@ -811,6 +811,12 @@ static void do_job( hb_job_t * job, int cpu_count )
     w->close( w );
     free( w );
 
+    hb_handle_t * h = job->h;
+    hb_state_t state;
+    hb_get_state( h, &state );
+    
+    hb_log("work: average encoding speed for job is %f fps", state.param.working.rate_avg);
+
 cleanup:
     /* Stop the write thread (thread_close will block until the muxer finishes) */
     if( job->muxer != NULL )
