dab4cbed725a789264014f8a5aeb19b3287b6195
sr55 <sr55.hb@outlook.com>
2009-05-26 22:32:34 +0000

    WinGui: - Missing Win32.cs File
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@2454 b64f7644-9d1e-0410-96f1-a4d463321fa5

---
+++ win/C#/Functions/Win32.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Handbrake.Functions
+{
+    class Win32
+    {
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(int hWnd);
+    }
+}
