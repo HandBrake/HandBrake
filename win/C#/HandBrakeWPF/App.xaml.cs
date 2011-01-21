5daa72facc91fad3d43ae16d151d737eca3faadb
sr55 <sr55.hb@outlook.com>
2011-01-21 21:51:39 +0000

    WinGui: - Add initial WPF project. (Uses MVVM, Caliburn 1.1)   This may one day replace the WinForms code.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@3762 b64f7644-9d1e-0410-96f1-a4d463321fa5

---
+++ win/C#/HandBrakeWPF/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿/*  App.xaml.cs $
+    This file is part of the HandBrake source code.
+    Homepage: <http://handbrake.fr>.
+    It may be used under the terms of the GNU General Public License. */
+
+namespace HandBrakeWPF
+{
+    using Caliburn.PresentationFramework.ApplicationModel;
+
+    using HandBrakeWPF.ViewModels;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : CaliburnApplication
+    {
+        /*
+         * TODO:
+         * - Setup Castle Windsor support for services.
+         * 
+         * 
+         */
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// </summary>
+        public App()
+        {
+        }
+
+        /// <summary>
+        /// Create the Root View
+        /// </summary>
+        /// <returns>
+        /// A MainViewMOdel
+        /// </returns>
+        protected override object CreateRootModel()
+        {
+            var binder = (DefaultBinder)Container.GetInstance<IBinder>();
+
+            binder.EnableBindingConventions();
+            binder.EnableMessageConventions();
+
+            return new MainViewModel();
+        }
+    }
+}
---
+++ win/C#/HandBrakeWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿/*  MainViewModel.cs $
+    This file is part of the HandBrake source code.
+    Homepage: <http://handbrake.fr>.
+    It may be used under the terms of the GNU General Public License. */
+
+namespace HandBrakeWPF.ViewModels
+{
+    using Caliburn.Core;
+
+    /// <summary>
+    /// HandBrakes Main Window
+    /// </summary>
+    public class MainViewModel : PropertyChangedBase
+    {
+        /// <summary>
+        /// HandBrakes Main Window Title
+        /// </summary>
+        private string windowName;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainViewModel"/> class.
+        /// </summary>
+        public MainViewModel()
+        {
+            this.WindowTitle = "HandBrake WPF Test Application";
+        }
+
+        /// <summary>
+        /// Gets or sets TestProperty.
+        /// </summary>
+        public string WindowTitle
+        {
+            get
+            {
+                return this.windowName;
+            }
+
+            set
+            {
+                if (!object.Equals(this.windowName, value))
+                {
+                    this.windowName = value;
+                    this.NotifyOfPropertyChange("TestProperty");
+                }
+            }
+        }
+    }
+}
