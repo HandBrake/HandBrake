0401bfcc021dafc27d20074c2c6080e5991f38cf
jstebbins <jstebbins.hb@gmail.com>
2011-05-27 15:12:33 +0000

    LinGui: Modify 0.9.5.1 branch to allow building on fedora 15 Add pkg-config test for dbus-glib and ifdef libnotify differences.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/tags/0.9.5.1@4007 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- gtk/src/callbacks.c
+++ gtk/src/callbacks.c
@@ -4889,9 +4889,13 @@ ghb_notify_done(signal_user_data_t *ud)
 	notification = notify_notification_new(
 		"Encode Complete",
 		"Put down that cocktail, Your HandBrake queue is done!",
-		"hb-icon",
-		NULL);
+		"hb-icon"
+#if NOTIFY_CHECK_VERSION(0, 7, 0)
+		);
+#else
+		,NULL);
 	notify_notification_attach_to_status_icon(notification, si);
+#endif
 	g_signal_connect(notification, "closed", (GCallback)notify_closed_cb, ud);
 	notify_notification_show(notification, NULL);
 #endif
