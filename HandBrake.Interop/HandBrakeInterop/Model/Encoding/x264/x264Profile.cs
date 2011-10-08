cdfffeec9b7431005f4975e220d844892f79ae4e
sr55 <sr55.hb@outlook.com>
2011-10-08 15:13:45 +0000

    Interop: Add some models for upcoming x264 preset/tune/profile & audio encoder ffflac.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk/win/CS@4271 b64f7644-9d1e-0410-96f1-a4d463321fa5

---
+++ HandBrake.Interop/HandBrakeInterop/Model/Encoding/x264/x264Profile.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="x264Profile.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   The X264 Profile
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrake.Interop.Model.Encoding.x264
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// The X264 Profile
+    /// </summary>
+    public enum x264Profile
+    {
+        [Display(Name = "Baseline")]
+        Baseline = 0,
+
+        [Display(Name = "Main")]
+        Main,
+
+        [Display(Name = "High")]
+        High,
+
+        [Display(Name = "High 10")]
+        High10,
+
+        [Display(Name = "High 422")]
+        High422,
+
+        [Display(Name = "High 444")]
+        High444,
+    }
+}
