fc44ec137ee00b31792dd54009eebefdbfb62c46
sr55 <sr55.hb@outlook.com>
2011-10-15 14:18:56 +0000

    WinGui: Fix a bug in the AutoName function when the source name has invalid characters for a file path.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk/win/CS@4288 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- Functions/Main.cs
+++ Functions/Main.cs
@@ -176,7 +176,8 @@ namespace Handbrake.Functions
             if (mainWindow.drp_dvdtitle.Text != "Automatic")
             {
                 // Get the Source Name and remove any invalid characters
-                string sourceName = Path.GetInvalidFileNameChars().Aggregate(Path.GetFileNameWithoutExtension(mainWindow.SourceName), (current, character) => current.Replace(character.ToString(), string.Empty));
+                string sourceName = Path.GetInvalidFileNameChars().Aggregate(mainWindow.SourceName, (current, character) => current.Replace(character.ToString(), string.Empty));
+                sourceName = Path.GetFileNameWithoutExtension(sourceName);
 
                 // Remove Underscores
                 if (UserSettingService.GetUserSetting<bool>(UserSettingConstants.AutoNameRemoveUnderscore))
