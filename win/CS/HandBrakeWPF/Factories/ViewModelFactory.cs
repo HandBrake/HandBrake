b1f4095b832276c031162d3f68ce8dfdeedc92ae
sr55 <sr55.hb@outlook.com>
2011-10-28 20:39:16 +0000

    WinGui: (WPF) Options window bindings are now in place.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@4326 b64f7644-9d1e-0410-96f1-a4d463321fa5

---
+++ win/CS/HandBrakeWPF/Factories/ViewModelFactory.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ViewModelFactory.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   The View Model Factory
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.Factories
+{
+    /// <summary>
+    /// The View Model Factory
+    /// </summary>
+    public class ViewModelFactory
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ViewModelFactory"/> class.
+        /// </summary>
+        public ViewModelFactory()
+        {
+        }
+    }
+}
