c5a5632e6b03b5281bd2b0ad35a4fcb7afffe364
sr55 <sr55.hb@outlook.com>
2011-10-29 20:17:24 +0000

    WinGui: (WPF) Continued work on the options window. Save Settings, Various functions and place holders for TODO's
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk/win/CS@4328 b64f7644-9d1e-0410-96f1-a4d463321fa5

---
+++ HandBrakeWPF/ViewModels/LogViewModel.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LogViewModel.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   Defines the LogViewModel type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.ViewModels
+{
+    using Caliburn.Micro;
+
+    /// <summary>
+    /// The Log View Model
+    /// </summary>
+    public class LogViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogViewModel"/> class.
+        /// </summary>
+        /// <param name="windowManager">
+        /// The window manager.
+        /// </param>
+        public LogViewModel(IWindowManager windowManager)
+            : base(windowManager)
+        {
+        }
+    }
+}
