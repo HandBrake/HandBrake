02ef15e82e63c3d4924fde8a03ef277ee4339785
sr55 <sr55.hb@outlook.com>
2011-11-20 20:02:49 +0000

    WinGui: (WPF) Some further UI Work.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@4362 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- win/CS/HandBrakeWPF/Views/Controls/AudioView.xaml.cs
+++ win/CS/HandBrakeWPF/Views/Controls/AudioView.xaml.cs
@@ -1,15 +1,64 @@
-﻿namespace HandBrakeWPF.Views.Controls
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AudioView.xaml.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   Interaction logic for AudioView.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.Views.Controls
 {
+    using System;
+    using System.ComponentModel;
+    using System.Windows;
     using System.Windows.Controls;
+
+    using HandBrake.ApplicationServices.Model.Encoding;
 
     /// <summary>
     /// Interaction logic for AudioView.xaml
     /// </summary>
     public partial class AudioView : UserControl
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AudioView"/> class.
+        /// </summary>
         public AudioView()
         {
             InitializeComponent();
+            this.AudioTracks = new BindingList<AudioTrack>();
+        }
+
+        /// <summary>
+        /// The "Query" Dependancy Property
+        /// </summary>
+        public static readonly DependencyProperty QueryProperty = DependencyProperty.Register("AudioTracks", typeof(BindingList<AudioTrack>), typeof(AudioView), new PropertyMetadata(null));
+
+
+        /// <summary>
+        /// Gets or sets State.
+        /// </summary>
+        public BindingList<AudioTrack> AudioTracks
+        {
+            get { return (BindingList<AudioTrack>)this.GetValue(QueryProperty); }
+            set { this.SetValue(QueryProperty, value); }
+        }
+
+        /// <summary>
+        /// Add an audio track.
+        /// </summary>
+        public void Add()
+        {
+            this.AudioTracks.Add(new AudioTrack());
+        }
+
+        /// <summary>
+        /// Remove an Audio Track
+        /// </summary>
+        public void Remove()
+        {
+            throw new NotImplementedException("Not Done Yet!");
         }
     }
 }
