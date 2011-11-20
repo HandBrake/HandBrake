71a45d588c4e0f9dfeefe54061eb78fbf3261c85
sr55 <sr55.hb@outlook.com>
2011-11-20 20:02:49 +0000

    WinGui: (WPF) Some further UI Work.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk/win/CS@4362 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- HandBrakeWPF/Views/Controls/AdvancedView.xaml.cs
+++ HandBrakeWPF/Views/Controls/AdvancedView.xaml.cs
@@ -1,5 +1,15 @@
-﻿namespace HandBrakeWPF.Views.Controls
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AdvancedView.xaml.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   Interaction logic for AdvancedView.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.Views.Controls
 {
+    using System.Windows;
     using System.Windows.Controls;
 
     /// <summary>
@@ -7,9 +17,26 @@
     /// </summary>
     public partial class AdvancedView : UserControl
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdvancedView"/> class.
+        /// </summary>
         public AdvancedView()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// The "Query" Dependancy Property
+        /// </summary>
+        public static readonly DependencyProperty QueryProperty = DependencyProperty.Register("Query", typeof(string), typeof(AdvancedView), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets or sets State.
+        /// </summary>
+        public string Query
+        {
+            get { return (string)this.GetValue(QueryProperty); }
+            set { this.SetValue(QueryProperty, value); }
         }
     }
 }
--- HandBrakeWPF/Views/Controls/ChaptersView.xaml.cs
+++ HandBrakeWPF/Views/Controls/ChaptersView.xaml.cs
@@ -1,15 +1,62 @@
-﻿namespace HandBrakeWPF.Views.Controls
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ChaptersView.xaml.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   Interaction logic for ChaptersView.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.Views.Controls
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Windows;
     using System.Windows.Controls;
+
+    using HandBrake.ApplicationServices.Parsing;
 
     /// <summary>
     /// Interaction logic for ChaptersView.xaml
     /// </summary>
     public partial class ChaptersView : UserControl
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChaptersView"/> class.
+        /// </summary>
         public ChaptersView()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// The "Query" Dependancy Property
+        /// </summary>
+        public static readonly DependencyProperty QueryProperty = DependencyProperty.Register("Chapters", typeof(IList<Chapter>), typeof(ChaptersView), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets or sets State.
+        /// </summary>
+        public IList<Chapter> Query
+        {
+            get { return (IList<Chapter>)this.GetValue(QueryProperty); }
+            set { this.SetValue(QueryProperty, value); }
+        }
+
+        /// <summary>
+        /// Import from CSV
+        /// </summary>
+        public void Import()
+        {
+            throw new NotImplementedException("Not Implemented Yet");
+        }
+
+        /// <summary>
+        /// Export to CSV
+        /// </summary>
+        public void Export()
+        {
+            throw new NotImplementedException("Not Implemented Yet");
         }
     }
 }
