044fa5acfdfba93e70f9ade5586f14aee0958532
sr55 <sr55.hb@outlook.com>
2011-11-26 22:45:40 +0000

    WinGui: (WPF) Further work on setting up the initial UI.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@4365 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- win/CS/HandBrakeWPF/Views/Controls/SubtitlesView.xaml.cs
+++ win/CS/HandBrakeWPF/Views/Controls/SubtitlesView.xaml.cs
@@ -1,15 +1,45 @@
-﻿namespace HandBrakeWPF.Views.Controls
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SubtitlesView.xaml.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   Interaction logic for SubtitlesView.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.Views.Controls
 {
+    using System.ComponentModel;
+    using System.Windows;
     using System.Windows.Controls;
+
+    using HandBrake.ApplicationServices.Model.Encoding;
 
     /// <summary>
     /// Interaction logic for SubtitlesView.xaml
     /// </summary>
     public partial class SubtitlesView : UserControl
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubtitlesView"/> class.
+        /// </summary>
         public SubtitlesView()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// The "Query" Dependancy Property
+        /// </summary>
+        public static readonly DependencyProperty QueryProperty = DependencyProperty.Register("SubtitleTracks", typeof(BindingList<SubtitleTrack>), typeof(SubtitlesView), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets or sets State.
+        /// </summary>
+        public BindingList<SubtitleTrack> AudioTracks
+        {
+            get { return (BindingList<SubtitleTrack>)this.GetValue(QueryProperty); }
+            set { this.SetValue(QueryProperty, value); }
         }
     }
 }
