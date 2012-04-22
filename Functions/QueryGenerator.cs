a59b5583db615907ffcc561258244d71e7b6f846
sr55 <sr55.hb@outlook.com>
2012-04-22 23:43:51 +0000

    WinGui: Always pass down the --previews flag for encodes when it's not defaulted to 10
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk/win/CS@4601 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- Functions/QueryGenerator.cs
+++ Functions/QueryGenerator.cs
@@ -235,7 +235,6 @@ namespace Handbrake.Functions
                     query += string.Format(" --start-at frame:{0} --stop-at frame:{1}", mainWindow.drop_chapterStart.Text, calculatedDuration);
                     break;
                 case 3: // Preview
-                    query += " --previews " + UserSettingService.GetUserSetting<int>(ASUserSettingConstants.PreviewScanCount) + " ";
                     query += " --start-at-preview " + preview;
                     query += " --stop-at duration:" + duration + " ";
                     break;
@@ -634,6 +633,13 @@ namespace Handbrake.Functions
             // LibDVDNav
             if (UserSettingService.GetUserSetting<bool>(ASUserSettingConstants.DisableLibDvdNav))
                 query += " --no-dvdnav";
+
+
+            if (UserSettingService.GetUserSetting<int>(ASUserSettingConstants.PreviewScanCount) != 10)
+            {
+                query += string.Format(
+                    " --previews {0} ", UserSettingService.GetUserSetting<int>(ASUserSettingConstants.PreviewScanCount));
+            }
 
             return query;
         }
