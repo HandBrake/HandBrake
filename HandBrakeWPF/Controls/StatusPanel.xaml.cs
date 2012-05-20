bb621f59cb20a0cb952a450e90c55c888a16ed97
sr55 <sr55.hb@outlook.com>
2012-05-20 02:07:56 +0000

    WinGui: Status overlay when scanning. Not sure if this is going to stay, just experimenting with ideas.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk/win/CS@4690 b64f7644-9d1e-0410-96f1-a4d463321fa5

---
+++ HandBrakeWPF/Controls/StatusPanel.xaml.cs
@@ -0,0 +1,73 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StatusPanel.xaml.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   Interaction logic for StatusPanel.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.Controls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for StatusPanel.xaml
+    /// </summary>
+    public partial class StatusPanel : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StatusPanel"/> class.
+        /// </summary>
+        public StatusPanel()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Dependancy Property for the IsLoading Property
+        /// </summary>
+        public static readonly DependencyProperty IsLoadingProperty =
+          DependencyProperty.Register("IsLoading", typeof(bool), typeof(StatusPanel), new UIPropertyMetadata(false));
+
+        /// <summary>
+        /// Dependancy Property for the Message Property
+        /// </summary>
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(StatusPanel), new UIPropertyMetadata(string.Empty));
+
+        /// <summary>
+        /// Dependancy Property for the submessage propery
+        /// </summary>
+        public static readonly DependencyProperty SubMessageProperty =
+            DependencyProperty.Register("SubMessage", typeof(string), typeof(StatusPanel), new UIPropertyMetadata(string.Empty));
+
+        /// <summary>
+        /// Gets or sets a value indicating whether IsLoading.
+        /// </summary>
+        public bool IsLoading
+        {
+            get { return (bool)GetValue(IsLoadingProperty); }
+            set { SetValue(IsLoadingProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets Message.
+        /// </summary>
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets SubMessage.
+        /// </summary>
+        public string SubMessage
+        {
+            get { return (string)GetValue(SubMessageProperty); }
+            set { SetValue(SubMessageProperty, value); }
+        }
+    }
+}
