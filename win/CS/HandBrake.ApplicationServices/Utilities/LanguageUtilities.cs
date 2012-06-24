8644f55a4e49256e684f92a30d50cd70a32339b4
sr55 <sr55.hb@outlook.com>
2012-06-24 12:30:28 +0000

    WinGui: Assorted Fixes
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@4773 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- win/CS/HandBrake.ApplicationServices/Utilities/LanguageUtilities.cs
+++ win/CS/HandBrake.ApplicationServices/Utilities/LanguageUtilities.cs
@@ -1,7 +1,11 @@
-﻿/*  LanguageUtilities.cs $
-    This file is part of the HandBrake source code.
-    Homepage: <http://handbrake.fr/>.
-    It may be used under the terms of the GNU General Public License. */
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LanguageUtilities.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   Language Utilities
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace HandBrake.ApplicationServices.Utilities
 {
