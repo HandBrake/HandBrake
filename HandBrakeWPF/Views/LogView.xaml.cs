b485abde0f66a342e067b9dab0aef77475ae54a7
sr55 <sr55.hb@outlook.com>
2012-06-30 17:37:25 +0000

    WinGui: Assorted fixes. - Implementation of CanBeBurned and CanBeForced on the subtitles panel. - Save updates to user presets from the Presets Options Menu. (Can use Add Preset to overwrite preset configuration instead also)
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk/win/CS@4800 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- HandBrakeWPF/Views/LogView.xaml.cs
+++ HandBrakeWPF/Views/LogView.xaml.cs
@@ -10,15 +10,41 @@
 namespace HandBrakeWPF.Views
 {
     using System.Windows;
+    using System.Windows.Controls;
 
     /// <summary>
     /// Interaction logic for LogView.xaml
     /// </summary>
     public partial class LogView : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogView"/> class.
+        /// </summary>
         public LogView()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Hide the Toolbar Endplate.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void ToolBarLoaded(object sender, RoutedEventArgs e)
+        {
+            ToolBar toolBar = sender as ToolBar;
+            if (toolBar != null)
+            {
+                var overflowGrid = toolBar.Template.FindName("OverflowGrid", toolBar) as FrameworkElement;
+                if (overflowGrid != null)
+                {
+                    overflowGrid.Visibility = Visibility.Collapsed;
+                }
+            }
         }
     }
 }
