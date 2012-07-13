6f99d2222606f13fa670d7da1b53766b466eca15
sr55 <sr55.hb@outlook.com>
2012-07-13 21:28:03 +0000

    WinGui: Improve the code behind the taskbar source menu. Note, The Disc insert detection still isn't in place, so it will only show the initial available drives, it won't detect disc changes yet.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk/win/CS@4828 b64f7644-9d1e-0410-96f1-a4d463321fa5

---
+++ HandBrakeWPF/Model/SourceMenuItem.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SourceMenuItem.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   Defines the SourceMenuItem type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.Model
+{
+    using System.Collections.ObjectModel;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// The source menu item.
+    /// </summary>
+    public class SourceMenuItem
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SourceMenuItem"/> class.
+        /// </summary>
+        public SourceMenuItem()
+        {
+            this.Children = new ObservableCollection<SourceMenuItem>();
+        }
+
+        /// <summary>
+        /// Gets or sets the text.
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Gets or sets the command.
+        /// </summary>
+        public ICommand Command { get; set; }
+
+        /// <summary>
+        /// Gets or sets the image.
+        /// </summary>
+        public Image Image { get; set; }
+
+        /// <summary>
+        /// Gets or sets the children.
+        /// </summary>
+        public ObservableCollection<SourceMenuItem> Children { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tag.
+        /// </summary>
+        public object Tag { get; set; }
+    }
+}
