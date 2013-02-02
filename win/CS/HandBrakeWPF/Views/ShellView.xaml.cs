81cf237836e97f9a0e23796da98f6411db783227
sr55 <sr55.hb@outlook.com>
2013-02-02 20:01:41 +0000

    WinGui: Add support for the tray icon.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@5235 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- win/CS/HandBrakeWPF/Views/ShellView.xaml.cs
+++ win/CS/HandBrakeWPF/Views/ShellView.xaml.cs
@@ -9,25 +9,57 @@
 
 namespace HandBrakeWPF.Views
 {
+    using System;
+    using System.IO;
     using System.Windows;
+    using System.Windows.Forms;
     using System.Windows.Input;
+    using System.Windows.Resources;
 
+    using Caliburn.Micro;
+
+    using HandBrake.ApplicationServices.Services.Interfaces;
     using HandBrake.ApplicationServices.Utilities;
 
     using HandBrakeWPF.Commands;
     using HandBrakeWPF.ViewModels.Interfaces;
 
+    using Application = System.Windows.Application;
+
     /// <summary>
     /// Interaction logic for ShellView.xaml
     /// </summary>
-    public partial class ShellView : Window
+    public partial class ShellView
     {
+        /// <summary>
+        /// The my notify icon.
+        /// </summary>
+        private readonly System.Windows.Forms.NotifyIcon notifyIcon;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ShellView"/> class.
         /// </summary>
         public ShellView()
         {
             this.InitializeComponent();
+
+            IUserSettingService userSettingService = IoC.Get<IUserSettingService>();
+            bool minimiseToTray = userSettingService.GetUserSetting<bool>(UserSettingConstants.MainWindowMinimize);
+
+            if (minimiseToTray)
+            {
+                this.notifyIcon = new System.Windows.Forms.NotifyIcon();
+                this.notifyIcon.ContextMenu = new ContextMenu(new[] { new MenuItem("Restore", NotifyIconClick) });
+
+                StreamResourceInfo streamResourceInfo = Application.GetResourceStream(new Uri("pack://application:,,,/handbrakepineapple.ico"));
+                if (streamResourceInfo != null)
+                {
+                    Stream iconStream = streamResourceInfo.Stream;
+                    this.notifyIcon.Icon = new System.Drawing.Icon(iconStream);
+                }
+                this.notifyIcon.DoubleClick += this.NotifyIconClick;
+                this.StateChanged += this.ShellViewStateChanged;
+            }
 
             // Start Encode (Ctrl+S)
             // Stop Encode (Ctrl+K)
@@ -53,6 +85,47 @@ namespace HandBrakeWPF.Views
         }
 
         /// <summary>
+        /// The notify icon_ click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void NotifyIconClick(object sender, EventArgs e)
+        {
+            this.WindowState = WindowState.Normal;
+        }
+
+        /// <summary>
+        /// The shell view state changed.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void ShellViewStateChanged(object sender, EventArgs e)
+        {
+            if (this.notifyIcon != null)
+            {
+                if (this.WindowState == WindowState.Minimized)
+                {
+                    this.ShowInTaskbar = false;
+                    notifyIcon.Visible = true;
+                    // notifyIcon.ShowBalloonTip(5000, "HandBrake", "Application Minimised", ToolTipIcon.Info);          
+                }
+                else if (this.WindowState == WindowState.Normal)
+                {
+                    notifyIcon.Visible = false;
+                    this.ShowInTaskbar = true;
+                }
+            }
+        }
+
+        /// <summary>
         /// Check with the user before closing.
         /// </summary>
         /// <param name="e">
@@ -69,6 +142,11 @@ namespace HandBrakeWPF.Views
                 {
                     e.Cancel = true;
                 }
+            }
+
+            if (this.notifyIcon != null)
+            {
+                this.notifyIcon.Visible = false;
             }
 
             base.OnClosing(e);
