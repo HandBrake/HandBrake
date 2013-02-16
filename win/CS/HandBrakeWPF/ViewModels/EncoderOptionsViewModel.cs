8fc8275d2b78f5212be0caf83f2a96bffa486817
sr55 <sr55.hb@outlook.com>
2013-02-16 20:12:16 +0000

    WinGui: Split out the advanced tab code into separate panels.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@5253 b64f7644-9d1e-0410-96f1-a4d463321fa5

---
+++ win/CS/HandBrakeWPF/ViewModels/EncoderOptionsViewModel.cs
@@ -0,0 +1,114 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EncoderOptionsViewModel.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   The Simple Encoder options screen
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.ViewModels
+{
+    using HandBrake.ApplicationServices.Model;
+    using HandBrake.ApplicationServices.Parsing;
+    using HandBrake.Interop.Model.Encoding;
+
+    using HandBrakeWPF.ViewModels.Interfaces;
+
+    /// <summary>
+    /// The Simple Encoder options screen
+    /// </summary>
+    public class EncoderOptionsViewModel : ViewModelBase, IEncoderOptionsViewModel, ITabInterface
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EncoderOptionsViewModel"/> class.
+        /// </summary>
+        public EncoderOptionsViewModel()
+        {
+            this.Task = new EncodeTask();
+        }
+
+        /// <summary>
+        /// Gets or sets the task.
+        /// </summary>
+        public EncodeTask Task { get; set; }
+
+        /// <summary>
+        /// Gets or sets the options string.
+        /// </summary>
+        public string AdvancedOptionsString
+        {
+            get
+            {
+                return this.Task.AdvancedEncoderOptions;
+            }
+            set
+            {
+                this.Task.AdvancedEncoderOptions = value;
+                this.NotifyOfPropertyChange(() => this.AdvancedOptionsString);
+            }
+        }
+
+        /// <summary>
+        /// The set source.
+        /// </summary>
+        /// <param name="selectedTitle">
+        /// The selected title.
+        /// </param>
+        /// <param name="currentPreset">
+        /// The current preset.
+        /// </param>
+        /// <param name="task">
+        /// The task.
+        /// </param>
+        public void SetSource(Title selectedTitle, Preset currentPreset, EncodeTask task)
+        {
+            this.Task = task;
+            this.NotifyOfPropertyChange(() => this.AdvancedOptionsString);
+        }
+
+        /// <summary>
+        /// The set preset.
+        /// </summary>
+        /// <param name="preset">
+        /// The preset.
+        /// </param>
+        /// <param name="task">
+        /// The task.
+        /// </param>
+        public void SetPreset(Preset preset, EncodeTask task)
+        {
+            this.Task = task;
+            this.AdvancedOptionsString = preset.Task.AdvancedEncoderOptions;
+        }
+
+        /// <summary>
+        /// The update task.
+        /// </summary>
+        /// <param name="task">
+        /// The task.
+        /// </param>
+        public void UpdateTask(EncodeTask task)
+        {        
+            this.Task = task;
+            this.NotifyOfPropertyChange(() => this.AdvancedOptionsString);
+        }
+
+        /// <summary>
+        /// The set encoder.
+        /// </summary>
+        /// <param name="encoder">
+        /// The encoder.
+        /// </param>
+        public void SetEncoder(VideoEncoder encoder)
+        {
+        }
+
+        /// <summary>
+        /// The clear.
+        /// </summary>
+        public void Clear()
+        {
+        }
+    }
+}
---
+++ win/CS/HandBrakeWPF/Views/EncoderOptionsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EncoderOptionsView.xaml.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   Interaction logic for EncoderOptionsView.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for AdvancedView.xaml
+    /// </summary>
+    public partial class EncoderOptionsView : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EncoderOptionsView"/> class. 
+        /// </summary>
+        public EncoderOptionsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
