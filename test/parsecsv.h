16477debabd0cd4bdd402529368d806b57a5170e
sr55 <sr55.hb@outlook.com>
2013-03-09 23:07:58 +0000

    Some further header updates.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@5322 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- test/parsecsv.h
+++ test/parsecsv.h
@@ -1,8 +1,11 @@
-/* $Id: parsecsv.h $
+/* parsecsv.c
 
-   This file is part of the HandBrake source code.
+   Copyright (c) 2003-2013 HandBrake Team
+   This file is part of the HandBrake source code
    Homepage: <http://handbrake.fr/>.
-   It may be used under the terms of the GNU General Public License. */
+   It may be used under the terms of the GNU General Public License v2.
+   For full terms see the file COPYING file or visit http://www.gnu.org/licenses/gpl-2.0.html
+ */
 
 /*
     A very simple CSV file parser.
