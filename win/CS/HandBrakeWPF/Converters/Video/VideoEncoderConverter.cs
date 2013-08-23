978227b1fa4e68005048b7ce3e4b81c432a2915d
sr55 <sr55.hb@outlook.com>
2013-08-23 22:03:05 +0000

    WinGui: Fix a small logic bug in the video encoder dropdown with  av_mkv
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@5743 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- win/CS/HandBrakeWPF/Converters/Video/VideoEncoderConverter.cs
+++ win/CS/HandBrakeWPF/Converters/Video/VideoEncoderConverter.cs
@@ -50,7 +50,7 @@ namespace HandBrakeWPF.Converters.Video
                 List<VideoEncoder> encoders = EnumHelper<VideoEncoder>.GetEnumList().ToList();
                 EncodeTask task = values[1] as EncodeTask;
 
-                if (task != null && (task.OutputFormat != OutputFormat.Mkv || task.OutputFormat != OutputFormat.av_mkv))
+                if (task != null && task.OutputFormat != OutputFormat.Mkv && task.OutputFormat != OutputFormat.av_mkv)
                 {
                     encoders.Remove(VideoEncoder.Theora);
                 }
