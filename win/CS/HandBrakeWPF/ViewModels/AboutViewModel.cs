d6f2df1984b93a44435acfe1cf34271c1cf9b924
sr55 <sr55.hb@outlook.com>
2013-09-08 14:17:50 +0000

    WinGui: Remove system info from about window.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@5777 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- win/CS/HandBrakeWPF/ViewModels/AboutViewModel.cs
+++ win/CS/HandBrakeWPF/ViewModels/AboutViewModel.cs
@@ -24,37 +24,11 @@ namespace HandBrakeWPF.ViewModels
     public class AboutViewModel : ViewModelBase, IAboutViewModel
     {
         /// <summary>
-        /// The system info.
-        /// </summary>
-        private string systemInfo;
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="AboutViewModel"/> class.
         /// </summary>
         public AboutViewModel()
         {
             this.Title = "About HandBrake";
-
-            StringBuilder builder = new StringBuilder();
-            foreach (var item in SystemInfo.GetGPUInfo)
-            {
-                builder.AppendLine(item);
-            }
-
-            StringBuilder system = new StringBuilder();
-            system.AppendLine(string.Format("Enviroment: {0}", Environment.NewLine));
-            system.AppendLine(string.Format("Operating System: {0}", Environment.OSVersion));
-            system.AppendLine(string.Format("CPU: {0}", SystemInfo.GetCpuCount));
-            system.AppendLine(string.Format("Ram: {0} MB{1}", SystemInfo.TotalPhysicalMemory, Environment.NewLine));
-
-            system.AppendLine(string.Format("{0}GPU Information:{0}{0}{1}", Environment.NewLine, builder));
-
-            system.AppendLine(string.Format("{0}System Paths:{0}", Environment.NewLine));
-            system.AppendLine(string.Format("Temp Dir: {0}", Path.GetTempPath()));
-            system.AppendLine(string.Format("Install Dir: {0}", Application.StartupPath));
-            system.AppendLine(string.Format("Data Dir: {0}\n", Application.UserAppDataPath));
-
-            SystemInformation = system.ToString();
         }
 
         /// <summary>
@@ -65,22 +39,6 @@ namespace HandBrakeWPF.ViewModels
             get
             {
                 return string.Format("{0} - {1}", VersionHelper.GetVersion(), VersionHelper.GetPlatformBitnessVersion());
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the system info.
-        /// </summary>
-        public string SystemInformation
-        {
-            get
-            {
-                return this.systemInfo;
-            }
-            set
-            {
-                this.systemInfo = value;
-                this.NotifyOfPropertyChange("SystemInfo");
             }
         }
 
