3d3dfd8db548a1506925e409b810dc7b9ba6ee41
Rodeo <tdskywalker@gmail.com>
2013-12-05 17:29:44 +0000

    sync: fix previous commit.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@5920 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- libhb/sync.c
+++ libhb/sync.c
@@ -1193,7 +1193,7 @@ static void InitAudio( hb_job_t * job, hb_sync_common_t * common, int i )
             } break;
             case HB_ACODEC_AAC_PASS:
             {
-                codec = avcodec_find_encoder_ny_name("aac");
+                codec = avcodec_find_encoder_by_name("aac");
             } break;
             default:
             {
