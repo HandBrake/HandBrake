e0944a5247a1deab5045abee60241b9fe7be7e40
sr55 <sr55.hb@outlook.com>
2014-08-18 20:01:40 +0000

    WinGui: Update copyright and version numbers for upcoming releases.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@6310 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- win/CS/HandBrake.Server/Properties/AssemblyInfo.cs
+++ win/CS/HandBrake.Server/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HandBrake Team")]
 [assembly: AssemblyProduct("HandBrake.Server")]
-[assembly: AssemblyCopyright("Copyright © 2013")]
+[assembly: AssemblyCopyright("Copyright © 2014 HandBrake Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.9.0")]
-[assembly: AssemblyFileVersion("0.9.9.0")]
+[assembly: AssemblyVersion("0.10.0.0")]
+[assembly: AssemblyFileVersion("0.10.0.0")]
