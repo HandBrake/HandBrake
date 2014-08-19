74a8a87f92d87cd324374cedb61f382f6407ba10
sr55 <sr55.hb@outlook.com>
2014-08-19 11:39:45 +0000

    WinGUI: Switch the appcast URL's to https
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@6314 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- win/CS/HandBrakeWPF/Constants.cs
+++ win/CS/HandBrakeWPF/Constants.cs
@@ -17,22 +17,22 @@ namespace HandBrakeWPF
         /// <summary>
         /// The appcast 64.
         /// </summary>
-        public const string Appcast64 = "http://handbrake.fr/appcast.x86_64.xml";
+        public const string Appcast64 = "https://handbrake.fr/appcast.x86_64.xml";
 
         /// <summary>
         /// The appcast 32.
         /// </summary>
-        public const string Appcast32 = "http://handbrake.fr/appcast.i386.xml";
+        public const string Appcast32 = "https://handbrake.fr/appcast.i386.xml";
 
         /// <summary>
         /// The appcast 32.
         /// </summary>
-        public const string AppcastUnstable32 = "http://handbrake.fr/appcast_unstable.i386.xml";
+        public const string AppcastUnstable32 = "https://handbrake.fr/appcast_unstable.i386.xml";
 
         /// <summary>
         /// The appcast 32.
         /// </summary>
-        public const string AppcastUnstable64 = "http://handbrake.fr/appcast_unstable.x86_64.xml";
+        public const string AppcastUnstable64 = "https://handbrake.fr/appcast_unstable.x86_64.xml";
 
         /// <summary>
         /// The any.
