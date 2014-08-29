59be70f507a3951cbb0707a706454d6f74fcb85a
jstebbins <jstebbins.hb@gmail.com>
2014-08-29 16:31:54 +0000

    libhb: add gpl v2 copyright message do deccc608sub
    much of this work was borrowed from ccextractor which is GPL v2
    
    
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@6383 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- libhb/deccc608sub.c
+++ libhb/deccc608sub.c
@@ -1,3 +1,12 @@
+/* deccc608sub.c
+
+   Copyright (c) 2003-2014 HandBrake Team
+   This file is part of the HandBrake source code
+   Homepage: <http://handbrake.fr/>.
+   It may be used under the terms of the GNU General Public License v2.
+   For full terms see the file COPYING file or visit http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
 /*
  * From ccextractor, leave this file as intact and close to the original as possible so that
  * it is easy to patch in fixes - even though this file contains code that we don't need.
--- libhb/deccc608sub.h
+++ libhb/deccc608sub.h
@@ -1,3 +1,12 @@
+/* deccc608sub.h
+
+   Copyright (c) 2003-2014 HandBrake Team
+   This file is part of the HandBrake source code
+   Homepage: <http://handbrake.fr/>.
+   It may be used under the terms of the GNU General Public License v2.
+   For full terms see the file COPYING file or visit http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
 /*
  * From ccextractor...
  */
