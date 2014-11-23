3ddf9cdc8af9d3586fe118038980462dbccdac01
ritsuka <damiog@gmail.com>
2014-11-23 12:56:03 +0000

    MacGui: moved the HBQueueOutlineView class to its own file.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@6539 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- macosx/HBQueueController.h
+++ macosx/HBQueueController.h
@@ -10,30 +10,6 @@
 
 @class HBController;
 
-//------------------------------------------------------------------------------------
-// As usual, we need to subclass NSOutlineView to handle a few special cases:
-//
-// (1) variable row heights during live resizes
-// HBQueueOutlineView exists solely to get around a bug in variable row height outline
-// views in which row heights get messed up during live resizes. See this discussion:
-// http://lists.apple.com/archives/cocoa-dev/2005/Oct/msg00871.html
-// However, the recommeneded fix (override drawRect:) does not work. Instead, this
-// subclass implements viewDidEndLiveResize in order to recalculate all row heights.
-//
-// (2) prevent expanding of items during drags
-// During dragging operations, we don't want outline items to expand, since a queue
-// doesn't really have children items.
-//
-// (3) generate a drag image that incorporates more than just the first column
-// By default, NSTableView only drags an image of the first column. Change this to
-// drag an image of the queue's icon and desc columns.
-
-@interface HBQueueOutlineView : NSOutlineView
-
-@property (nonatomic, readonly) BOOL isDragging;
-
-@end
-
 @interface HBQueueController : NSWindowController <NSToolbarDelegate, NSWindowDelegate>
 
 - (void)setPidNum: (int)myPidnum;
@@ -49,10 +25,5 @@
 - (void)setQueueStatusString: (NSString *)statusString;
 
 - (IBAction)showQueueWindow: (id)sender;
-
-/* control encodes in the window */
-- (IBAction)removeSelectedQueueItem: (id)sender;
-- (IBAction)revealSelectedQueueItem: (id)sender;
-- (IBAction)editSelectedQueueItem: (id)sender;
 
 @end
--- macosx/HBQueueController.mm
+++ macosx/HBQueueController.mm
@@ -6,6 +6,8 @@
 
 #import "HBQueueController.h"
 #import "Controller.h"
+
+#import "HBQueueOutlineView.h"
 #import "HBImageAndTextCell.h"
 #import "HBUtilities.h"
 
@@ -33,96 +35,9 @@
 }
 @end
 
-
-@implementation HBQueueOutlineView
-
-- (void)viewDidEndLiveResize
-{
-    // Since we disabled calculating row heights during a live resize, force them to
-    // recalculate now.
-    [self noteHeightOfRowsWithIndexesChanged:
-            [NSIndexSet indexSetWithIndexesInRange: NSMakeRange(0, [self numberOfRows])]];
-    [super viewDidEndLiveResize];
-}
-
-/* This should be for dragging, we take this info from the presets right now */
-- (NSImage *)dragImageForRowsWithIndexes:(NSIndexSet *)dragRows
-                            tableColumns:(NSArray *)tableColumns
-                                   event:(NSEvent *)dragEvent
-                                  offset:(NSPointPointer)dragImageOffset
-{
-    _isDragging = YES;
-
-    // By default, NSTableView only drags an image of the first column. Change this to
-    // drag an image of the queue's icon and desc and action columns.
-    NSArray * cols = @[[self tableColumnWithIdentifier:@"desc"], [self tableColumnWithIdentifier:@"icon"],[self tableColumnWithIdentifier:@"action"]];
-    return [super dragImageForRowsWithIndexes:dragRows tableColumns:cols event:dragEvent offset:dragImageOffset];
-}
-
-- (void)mouseDown:(NSEvent *)theEvent
-{
-    [super mouseDown:theEvent];
-	_isDragging = NO;
-}
-
-- (void)keyDown:(NSEvent *)event
-{
-    id delegate = [self delegate];
-
-    unichar key = [[event charactersIgnoringModifiers] characterAtIndex:0];
-    if ((key == NSDeleteCharacter || key == NSDeleteFunctionKey) &&
-               [delegate respondsToSelector:@selector(removeSelectedQueueItem:)])
-    {
-        if ([self selectedRow] == -1)
-        {
-            NSBeep();
-        }
-        else
-        {
-            [delegate removeSelectedQueueItem:self];
-        }
-        return;
-    }
-    else
-    {
-        [super keyDown:event];
-    }
-}
-
-/**
- *  An index set containing the indexes of the targeted rows.
- *  If the selected row indexes contain the clicked row index, it returns every selected row,
- *  otherwise it returns only the clicked row index.
- */
-- (NSIndexSet *)targetedRowIndexes
-{
-    NSMutableIndexSet *rowIndexes = [NSMutableIndexSet indexSet];
-    NSIndexSet *selectedRowIndexes = [self selectedRowIndexes];
-    NSInteger clickedRow = [self clickedRow];
-
-    if (clickedRow != -1)
-    {
-        [rowIndexes addIndex:clickedRow];
-
-        // If we clicked on a selected row, then we want to consider all rows in the selection. Otherwise, we only consider the clicked on row.
-        if ([selectedRowIndexes containsIndex:clickedRow])
-        {
-            [rowIndexes addIndexes:selectedRowIndexes];
-        }
-    }
-    else
-    {
-        [rowIndexes addIndexes:selectedRowIndexes];
-    }
-
-    return [[rowIndexes copy] autorelease];
-}
-
-@end
-
 #pragma mark -
 
-@interface HBQueueController ()
+@interface HBQueueController () <HBQueueOutlineViewDelegate>
 {
     hb_handle_t                  *fQueueEncodeLibhb;              // reference to libhb
     HBController                 *fHBController;        // reference to HBController
@@ -154,6 +69,11 @@
     NSDictionary            *titleAttr;
     NSDictionary            *shortHeightAttr;
 }
+
+/* control encodes in the window */
+- (IBAction)removeSelectedQueueItem: (id)sender;
+- (IBAction)revealSelectedQueueItem: (id)sender;
+- (IBAction)editSelectedQueueItem: (id)sender;
 
 @end
 
@@ -414,6 +334,12 @@
 
 
 #pragma mark Queue Item Controls
+
+- (void)HB_deleteSelectionFromTableView:(NSTableView *)tableView
+{
+    [self removeSelectedQueueItem:tableView];
+}
+
 //------------------------------------------------------------------------------------
 // Delete encodes from the queue window and accompanying array
 // Also handling first cancelling the encode if in fact its currently encoding.
