7671dfa2e3f63807d99b9e3f639f4609afe9431d
sr55 <sr55.hb@outlook.com>
2015-01-09 21:36:21 +0000

    WinGui: Another copyright notice updated.
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@6710 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- win/CS/HandBrakeWPF/Properties/Resources.Designer.cs
+++ win/CS/HandBrakeWPF/Properties/Resources.Designer.cs
@@ -61,7 +61,7 @@ namespace HandBrakeWPF.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Copyright (C) 2003-2014 The HandBrake Team
+        ///   Looks up a localized string similar to Copyright (C) 2003-2015 The HandBrake Team
         ///
         ///This program is free software; you can redistribute it and/or
         ///modify it under the terms of the GNU General Public License
