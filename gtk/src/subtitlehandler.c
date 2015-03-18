3e026ffb7dab010cad55c17977a3c15ed7df58c3
jstebbins <jstebbins.hb@gmail.com>
2015-03-18 21:20:06 +0000

    LinGui: fix off-by-one error in subtitle track selection
    git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@7003 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- gtk/src/subtitlehandler.c
+++ gtk/src/subtitlehandler.c
@@ -552,7 +552,7 @@ ghb_set_pref_subtitle_settings(signal_user_data_t *ud, const hb_title_t *title, 
         // is foreign language.
         foreign_audio_search = FALSE;
         track = ghb_find_subtitle_track(title, pref_lang, 0);
-        if (track > 0)
+        if (track >= 0)
         {
             gboolean burn;
             hb_subtitle_t *subtitle;
