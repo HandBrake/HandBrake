742e32b875a2e6a7c879ba0a84fb7512608c25fd
Damiano Galassi <damiog@gmail.com>
2015-10-15 19:38:44 +0200

    MacGui: do not try to import a preset even if the cancel button was clicked.

--- macosx/HBPresetsViewController.m
+++ macosx/HBPresetsViewController.m
@@ -101,7 +101,7 @@
 
     [panel beginWithCompletionHandler:^(NSInteger result)
      {
-         if (result == NSOKButton)
+         if (result == NSFileHandlingPanelOKButton)
          {
              NSURL *presetExportDirectory = [panel.URL URLByDeletingLastPathComponent];
              [[NSUserDefaults standardUserDefaults] setURL:presetExportDirectory forKey:@"LastPresetExportDirectoryURL"];
@@ -133,19 +133,22 @@
      {
          [[NSUserDefaults standardUserDefaults] setURL:panel.directoryURL forKey:@"LastPresetImportDirectoryURL"];
 
-         for (NSURL *url in panel.URLs)
+         if (result == NSFileHandlingPanelOKButton)
          {
-             NSError *error;
-             HBPreset *import = [[HBPreset alloc] initWithContentsOfURL:url error:&error];
-
-             if (import == nil)
+             for (NSURL *url in panel.URLs)
              {
-                 [self presentError:error];
-             }
+                 NSError *error;
+                 HBPreset *import = [[HBPreset alloc] initWithContentsOfURL:url error:&error];
 
-             for (HBPreset *child in import.children)
-             {
-                 [self.presets addPreset:child];
+                 if (import == nil)
+                 {
+                     [self presentError:error];
+                 }
+
+                 for (HBPreset *child in import.children)
+                 {
+                     [self.presets addPreset:child];
+                 }
              }
          }
      }];
