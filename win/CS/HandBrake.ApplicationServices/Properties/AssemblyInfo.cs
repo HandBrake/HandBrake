70c171d88da2cc6e8f45c245e048b8b445e208f4
sr55 <sr55.hb@outlook.com>
2015-12-14 20:37:51 +0000

    WinGui: Another attempt to fix the 0.10.x build

---
+++ win/CS/HandBrake.ApplicationServices/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HandBrake")]
+[assembly: AssemblyDescription("HandBrake is an open-source, GPL-licensed, multiplatform, video transcoder.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HandBrake Team")]
+[assembly: AssemblyProduct("HandBrake")]
+[assembly: AssemblyCopyright("Copyright © 2003-2015 HandBrake Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("80ba130e-73a6-4c35-8f82-a6bd4f0ff4d2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.10.3.0")]
+[assembly: AssemblyFileVersion("0.10.3.0")]
+[assembly: NeutralResourcesLanguage("en-GB")]
+
