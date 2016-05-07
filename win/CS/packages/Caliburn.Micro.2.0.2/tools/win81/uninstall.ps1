214d14d0011c0c6c43b51c53240dea3ced8fb174
RandomEngy <david.rickard@gmail.com>
2016-05-06 18:39:47 -0700

    Interop: Changed to use NuGet (take 2)
    Now including packages in repo as they are needed for build server.

---
+++ win/CS/packages/Caliburn.Micro.2.0.2/tools/win81/uninstall.ps1
@@ -0,0 +1,2 @@
+param($installPath, $toolsPath, $package, $project)
+Write-Host "Caliburn.Micro was removed, however Blend SDK (XAML) is still being referenced, if you do not need it you can manually remove it."
