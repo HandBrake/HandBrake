8964f5b518a16b4b19494b6f138f113f7f27bfb7
John Stebbins <jstebbins.hb@gmail.com>
2016-05-18 09:19:20 -0600

    sync: set start and stop when resampling audio
    It's not strictly necessary because it gets done elsewhere as well.  But
    putting it here makes the code more understandable.

--- libhb/sync.c
+++ libhb/sync.c
@@ -2314,6 +2314,8 @@ static hb_buffer_t * FilterAudioFrame( sync_stream_t * stream,
             }
             buf->s.duration = 90000. * stream->audio.src.pkt.output_frames_gen /
                               audio->config.out.samplerate;
+            buf->s.start = stream->next_pts;
+            buf->s.stop = stream->next_pts + buf->s.duration;
         }
         if (audio->config.out.gain > 0.0)
         {
