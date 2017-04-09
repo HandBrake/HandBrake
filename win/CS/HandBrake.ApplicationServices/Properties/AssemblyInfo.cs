47819a1cd5dbba570290c87d943ccd0c23fc759e
sr55 <sr55.hb@outlook.com>
2017-04-09 20:03:41 +0100

    WinGui: Bump Version numbers to 1.0.5

--- win/CS/HandBrake.ApplicationServices/Properties/AssemblyInfo.cs
+++ win/CS/HandBrake.ApplicationServices/Properties/AssemblyInfo.cs
@@ -43,5 +43,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
 [assembly: NeutralResourcesLanguage("")]
