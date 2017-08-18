a5d09db0caec7c880e9b280d72a2fafeb122815b
sr55 <sr55.hb@outlook.com>
2017-08-18 20:39:51 +0100

    WinGui: Give the QueueTask object a distinct surrogate key  #854

--- win/CS/HandBrakeWPF/Services/Queue/Model/QueueTask.cs
+++ win/CS/HandBrakeWPF/Services/Queue/Model/QueueTask.cs
@@ -20,14 +20,8 @@ namespace HandBrakeWPF.Services.Queue.Model
     /// </summary>
     public class QueueTask : PropertyChangedBase
     {
-        #region Constants and Fields
-
-        /// <summary>
-        /// The status.
-        /// </summary>
+        private static int id = 0;
         private QueueItemStatus status;
-
-        #endregion
 
         #region Properties
 
@@ -37,6 +31,8 @@ namespace HandBrakeWPF.Services.Queue.Model
         public QueueTask()
         {
             this.Status = QueueItemStatus.Waiting;
+            id = id + 1;
+            this.Id = id;
         }
 
         /// <summary>
@@ -57,12 +53,17 @@ namespace HandBrakeWPF.Services.Queue.Model
             this.Configuration = configuration;
             this.Status = QueueItemStatus.Waiting;
             this.ScannedSourcePath = scannedSourcePath;
+
+            id = id + 1;
+            this.Id = id;
         }
+
+        public int Id { get; private set; }
 
         /// <summary>
         /// Gets or sets ScannedSource.
         /// </summary>
-        public string ScannedSourcePath { get; set; } 
+        public string ScannedSourcePath { get; set; }
 
         /// <summary>
         /// Gets or sets Status.
@@ -93,64 +94,22 @@ namespace HandBrakeWPF.Services.Queue.Model
 
         #endregion
 
-        /// <summary>
-        /// The equals.
-        /// </summary>
-        /// <param name="obj">
-        /// The obj.
-        /// </param>
-        /// <returns>
-        /// The <see cref="bool"/>.
-        /// </returns>
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj))
-            {
-                return false;
-            }
-
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-
-            if (obj.GetType() != this.GetType())
-            {
-                return false;
-            }
-
-            return this.Equals((QueueTask)obj);
-        }
-
-        /// <summary>
-        /// The get hash code.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="int"/>.
-        /// </returns>
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hashCode = (this.ScannedSourcePath != null ? this.ScannedSourcePath.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (this.Task != null ? this.Task.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (int)this.status;
-                return hashCode;
-            }
-        }
-
-        /// <summary>
-        /// The equals.
-        /// </summary>
-        /// <param name="other">
-        /// The other.
-        /// </param>
-        /// <returns>
-        /// The <see cref="bool"/>.
-        /// </returns>
         protected bool Equals(QueueTask other)
         {
-            return Equals(this.ScannedSourcePath, other.ScannedSourcePath) && Equals(this.Task, other.Task) && this.status == other.status;
+            return this.Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((QueueTask)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id;
         }
     }
 }
