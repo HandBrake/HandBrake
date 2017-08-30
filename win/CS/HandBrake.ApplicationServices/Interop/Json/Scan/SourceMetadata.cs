43dc0cdb9d18c43771ca8f9ed52b19550c2c1b55
sr55 <sr55.hb@outlook.com>
2017-08-30 21:41:29 +0100

    WinGui: Complete the SourceMetadata object.

--- win/CS/HandBrake.ApplicationServices/Interop/Json/Scan/SourceMetadata.cs
+++ win/CS/HandBrake.ApplicationServices/Interop/Json/Scan/SourceMetadata.cs
@@ -14,5 +14,14 @@ namespace HandBrake.ApplicationServices.Interop.Json.Scan
     /// </summary>
     public class SourceMetadata
     {
+        public string Artist { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public string LongDescription { get; set; }
+        public string Name { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Composer { get; set; }
+        public string AlbumArtist { get; set; }
+        public string Comment { get; set; }
     }
 }
