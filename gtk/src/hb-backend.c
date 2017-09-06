bb3235e43de4c8bafd24fb25391e506005c796e2
John Stebbins <jstebbins.hb@gmail.com>
2017-09-06 10:09:43 -0700

    LinGui: Add filter validation for sharpen and comb detect

--- gtk/src/hb-backend.c
+++ gtk/src/hb-backend.c
@@ -3903,6 +3903,72 @@ ghb_validate_filters(GhbValue *settings, GtkWindow *parent)
 {
     gchar *message;
 
+    // Detelecine
+    const char *detel_preset;
+    detel_preset = ghb_dict_get_string(settings, "PictureDetelecine");
+    if (strcasecmp(detel_preset, "off"))
+    {
+        const char *detel_custom = NULL;
+        int filter_id;
+
+        filter_id = HB_FILTER_DETELECINE;
+        detel_custom = ghb_dict_get_string(settings, "PictureDetelecineCustom");
+        if (hb_validate_filter_preset(filter_id, detel_preset, NULL,
+                                      detel_custom))
+        {
+            if (detel_custom != NULL)
+            {
+                message = g_strdup_printf(
+                            _("Invalid Detelecine Settings:\n\n"
+                              "Preset:\t%s\n"
+                              "Custom:\t%s\n"), detel_preset, detel_custom);
+            }
+            else
+            {
+                message = g_strdup_printf(
+                            _("Invalid Detelecine Settings:\n\n"
+                              "Preset:\t%s\n"), detel_preset);
+            }
+            ghb_message_dialog(parent, GTK_MESSAGE_ERROR,
+                               message, _("Cancel"), NULL);
+            g_free(message);
+            return FALSE;
+        }
+    }
+
+    // Comb Detect
+    const char *comb_preset;
+    comb_preset = ghb_dict_get_string(settings, "PictureCombDetectPreset");
+    if (strcasecmp(comb_preset, "off"))
+    {
+        const char *comb_custom = NULL;
+        int filter_id;
+
+        filter_id = HB_FILTER_COMB_DETECT;
+        comb_custom = ghb_dict_get_string(settings, "PictureCombDetectCustom");
+        if (hb_validate_filter_preset(filter_id, comb_preset, NULL,
+                                      comb_custom))
+        {
+            if (comb_custom != NULL && comb_custom[0] != 0)
+            {
+                message = g_strdup_printf(
+                            _("Invalid Comb Detect Settings:\n\n"
+                              "Preset:\t%s\n"
+                              "Custom:\t%s\n"), comb_preset, comb_custom);
+            }
+            else
+            {
+                message = g_strdup_printf(
+                            _("Invalid Comb Detect Settings:\n\n"
+                              "Preset:\t%s\n"), comb_preset);
+            }
+            ghb_message_dialog(parent, GTK_MESSAGE_ERROR,
+                               message, _("Cancel"), NULL);
+            g_free(message);
+            return FALSE;
+        }
+    }
+
     // Deinterlace
     int filter_id;
     filter_id = ghb_settings_combo_int(settings, "PictureDeinterlaceFilter");
@@ -3923,50 +3989,17 @@ ghb_validate_filters(GhbValue *settings, GtkWindow *parent)
             {
                 message = g_strdup_printf(
                             _("Invalid Deinterlace Settings:\n\n"
-                              "Filter: %s\n"
-                              "Preset: %s\n"
-                              "Custom: %s\n"), deint_filter, deint_preset,
-                                               deint_custom);
+                              "Filter:\t%s\n"
+                              "Preset:\t%s\n"
+                              "Custom:\t%s\n"), deint_filter, deint_preset,
+                                                deint_custom);
             }
             else
             {
                 message = g_strdup_printf(
                             _("Invalid Deinterlace Settings:\n\n"
-                              "Filter: %s\n"
-                              "Preset: %s\n"), deint_filter, deint_preset);
-            }
-            ghb_message_dialog(parent, GTK_MESSAGE_ERROR,
-                               message, _("Cancel"), NULL);
-            g_free(message);
-            return FALSE;
-        }
-    }
-
-    // Detelecine
-    const char *detel_preset;
-    detel_preset = ghb_dict_get_string(settings, "PictureDetelecine");
-    if (strcasecmp(detel_preset, "off"))
-    {
-        const char *detel_custom = NULL;
-        int filter_id;
-
-        filter_id = HB_FILTER_DETELECINE;
-        detel_custom = ghb_dict_get_string(settings, "PictureDetelecineCustom");
-        if (hb_validate_filter_preset(filter_id, detel_preset, NULL,
-                                      detel_custom))
-        {
-            if (detel_custom != NULL)
-            {
-                message = g_strdup_printf(
-                            _("Invalid Detelecine Settings:\n\n"
-                              "Preset: %s\n"
-                              "Custom: %s\n"), detel_preset, detel_custom);
-            }
-            else
-            {
-                message = g_strdup_printf(
-                            _("Invalid Detelecine Settings:\n\n"
-                              "Preset: %s\n"), detel_preset);
+                              "Filter:\t%s\n"
+                              "Preset:\t%s\n"), deint_filter, deint_preset);
             }
             ghb_message_dialog(parent, GTK_MESSAGE_ERROR,
                                message, _("Cancel"), NULL);
@@ -3994,11 +4027,39 @@ ghb_validate_filters(GhbValue *settings, GtkWindow *parent)
         {
             message = g_strdup_printf(
                         _("Invalid Denoise Settings:\n\n"
-                          "Filter: %s\n"
-                          "Preset: %s\n"
-                          "Tune:   %s\n"
-                          "Custom: %s\n"), denoise_filter, denoise_preset,
+                          "Filter:\t%s\n"
+                          "Preset:\t%s\n"
+                          "Tune:\t%s\n"
+                          "Custom:\t%s\n"), denoise_filter, denoise_preset,
                                            denoise_tune, denoise_custom);
+            ghb_message_dialog(parent, GTK_MESSAGE_ERROR,
+                               message, _("Cancel"), NULL);
+            g_free(message);
+            return FALSE;
+        }
+    }
+
+    // Sharpen
+    filter_id = ghb_settings_combo_int(settings, "PictureSharpenFilter");
+    if (filter_id != HB_FILTER_INVALID)
+    {
+        const char *sharpen_filter, *sharpen_preset;
+        const char *sharpen_tune = NULL, *sharpen_custom = NULL;
+
+        sharpen_filter = ghb_dict_get_string(settings, "PictureSharpenFilter");
+        sharpen_preset = ghb_dict_get_string(settings, "PictureSharpenPreset");
+        sharpen_tune = ghb_dict_get_string(settings, "PictureSharpenTune");
+        sharpen_custom = ghb_dict_get_string(settings, "PictureSharpenCustom");
+        if (hb_validate_filter_preset(filter_id, sharpen_preset, sharpen_tune,
+                                      sharpen_custom))
+        {
+            message = g_strdup_printf(
+                        _("Invalid Sharpen Settings:\n\n"
+                          "Filter:\t%s\n"
+                          "Preset:\t%s\n"
+                          "Tune:\t%s\n"
+                          "Custom:\t%s\n"), sharpen_filter, sharpen_preset,
+                                           sharpen_tune, sharpen_custom);
             ghb_message_dialog(parent, GTK_MESSAGE_ERROR,
                                message, _("Cancel"), NULL);
             g_free(message);
