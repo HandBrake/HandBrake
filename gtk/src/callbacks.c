d834541e95baf5dc8e91e2ff80fac55f7e83a183
Matthew Lazarow <Matthew.Lazarow@gmail.com>
2017-09-12 21:10:11 -0400

    Added the nvenc entries to the switch statement just like VP8/9

--- gtk/src/callbacks.c
+++ gtk/src/callbacks.c
@@ -5203,6 +5203,8 @@ format_vquality_cb(GtkScale *scale, gdouble val, signal_user_data_t *ud)
         case HB_VCODEC_FFMPEG_MPEG2:
         case HB_VCODEC_FFMPEG_VP8:
         case HB_VCODEC_FFMPEG_VP9:
+        case HB_VCODEC_H264_NVENC:
+        case HB_VCODEC_HEVC_NVENC:
         case HB_VCODEC_THEORA:
         {
             return g_strdup_printf("%s: %d", vqname, (int)val);
