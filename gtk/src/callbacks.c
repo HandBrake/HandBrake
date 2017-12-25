c3c12e41684718db32c7ba6bdc2acc703468d91f
Yuichiro NAITO <naito.yuichiro@gmail.com>
2017-12-25 16:36:26 +0900

    socket(2) is defined in sys/socket.h in FreeBSD.

--- gtk/src/callbacks.c
+++ gtk/src/callbacks.c
@@ -46,6 +46,9 @@
 #include <dbus/dbus-glib.h>
 #include <dbus/dbus-glib-lowlevel.h>
 
+#if defined( __FreeBSD__ )
+#include <sys/socket.h>
+#endif
 #include <netinet/in.h>
 #include <netdb.h>
 
@@ -5865,4 +5868,3 @@ void ghb_container_empty(GtkContainer *c)
 {
     gtk_container_foreach(c, container_empty_cb, NULL);
 }
-
