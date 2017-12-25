2bf298409dc7ec2f9a686a1d46110ac880227322
Sven Gothel <sgothel@jausoft.com>
2017-12-26 00:28:51 +0100

    NVENC: Remaining fixes for 1st GNU/Linux GTK working draft
    - Fix HB_VCODEC_FFMPEG_MASK covered values to lie within mask
    - Fix HB_VCODEC_FFMPEG_H264 usage (gtk version missed a rename)
    - Fix FFMPEG hwaccel build is required for Nvenc
    
    Basically working:
      - h264_nvenc
      - hevc_nvenc
        Possible optimization:
      - Even in constant quality mode, a user ceiling bit_rate might be desirable,
        currently only the maximum is used like in VP8/9: w x h x fps
    
      Remaining issues:
        1) Muxer issue with h264_nvenc + MKV, ffprobe shows:
    
        [h264 @ 0x55ae4dedacc0] sps_id 32 out of range
        [h264 @ 0x55ae4dedacc0] Invalid nal size 1677734572
        [h264 @ 0x55ae4dedacc0] missing picture in access unit with size 40644
    
         Notable: This does not happen with hevc_nvenc
    
      Tested:
        - GNU/Linux Debian 9
        - GeForce GTX 950
        - BD 1080p source, cropped: Around 70fps h264 + hevc

--- libhb/work.c
+++ libhb/work.c
@@ -482,6 +482,8 @@ void hb_display_job_info(hb_job_t *job)
                 case HB_VCODEC_QSV_H264:
                 case HB_VCODEC_QSV_H265:
                 case HB_VCODEC_QSV_H265_10BIT:
+                case HB_VCODEC_FFMPEG_H264:
+                case HB_VCODEC_FFMPEG_H265:
                     hb_log("     + profile: %s", job->encoder_profile);
                 default:
                     break;
@@ -496,6 +498,8 @@ void hb_display_job_info(hb_job_t *job)
                 case HB_VCODEC_QSV_H264:
                 case HB_VCODEC_QSV_H265:
                 case HB_VCODEC_QSV_H265_10BIT:
+                case HB_VCODEC_FFMPEG_H264:
+                case HB_VCODEC_FFMPEG_H265:
                     hb_log("     + level:   %s", job->encoder_level);
                 default:
                     break;
