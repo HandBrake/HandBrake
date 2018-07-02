f12ebd45ef608ccc75b8e4382e58aafe414831d8
Yuichiro NAITO <naito.yuichiro@gmail.com>
2018-07-02 22:57:41 +0900

    build: Fix to not pass --enable-nvenc to FFmpeg configure on FreeBSD.
    Nvenc is not currently supported by Nvidia on FreeBSD. If it becomes supported on FreeBSD, please check nvidia-driver and FFmpeg.

--- make/configure.py
+++ make/configure.py
@@ -1320,7 +1320,7 @@ def createCLI():
     grp.add_option( '--disable-ffmpeg-aac', dest="enable_ffmpeg_aac", action='store_false' )
 
     h = IfHost( 'enable Nvidia NVEnc video encoder', '*-*-*', none=optparse.SUPPRESS_HELP ).value
-    grp.add_option( '--enable-nvenc', dest="enable_nvenc", default=not host.match( '*-*-darwin*' ), action='store_true', help=h )
+    grp.add_option( '--enable-nvenc', dest="enable_nvenc", default=not (host.match( '*-*-darwin*' ) or host.match( '*-*-freebsd*' )), action='store_true', help=h )
 
 
     cli.add_option_group( grp )
