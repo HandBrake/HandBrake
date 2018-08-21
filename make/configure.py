7767d0f28de1accae535d422cd295c46f0815c7d
Yuichiro NAITO <naito.yuichiro@gmail.com>
2018-08-21 10:42:30 +0900

    build: Allows to use clang/clang++ compiler on FreeBSD.
    LOCALBASE variable is used to indicate where FreeBSD ports are installed.
    default is /usr/local.

--- make/configure.py
+++ make/configure.py
@@ -1512,7 +1512,7 @@ try:
     class Tools:
         ar    = ToolProbe( 'AR.exe',    'ar', abort=True )
         cp    = ToolProbe( 'CP.exe',    'cp', abort=True )
-        gcc   = ToolProbe( 'GCC.gcc',   'gcc', IfHost( 'gcc-4', '*-*-cygwin*' ))
+        gcc   = ToolProbe( 'GCC.gcc',   'gcc', IfHost( 'clang', '*-*-freebsd*' ), IfHost( 'gcc-4', '*-*-cygwin*' ))
 
         if host.match( '*-*-darwin*' ):
             gmake = ToolProbe( 'GMAKE.exe', 'make', 'gmake', abort=True )
