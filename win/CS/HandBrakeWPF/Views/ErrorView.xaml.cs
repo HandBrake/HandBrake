c332cf6f061948c2f03c908faca59fa05d1aead4
sr55 <sr55.hb@outlook.com>
2019-03-09 20:50:06 +0000

    WinGui: Bind "Ctrl-C" on the Error window. This *may* be usable in some instances where the screen doesn't render correctly to allow us to see the error.  #1950

--- win/CS/HandBrakeWPF/Views/ErrorView.xaml.cs
+++ win/CS/HandBrakeWPF/Views/ErrorView.xaml.cs
@@ -9,19 +9,45 @@
 
 namespace HandBrakeWPF.Views
 {
+    using System;
     using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
 
-    /// <summary>
-    /// Interaction logic for ErrorView.xaml
-    /// </summary>
+    using HandBrakeWPF.Commands;
+    using HandBrakeWPF.ViewModels.Interfaces;
+
     public partial class ErrorView : Window
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ErrorView"/> class.
-        /// </summary>
         public ErrorView()
-        {
-            InitializeComponent();
+        {  
+            this.InitializeComponent();
+            this.InputBindings.Add(new InputBinding(new CopyError(this.errorText), new KeyGesture(Key.C, ModifierKeys.Control))); // Copy Error
         }
+    }
+
+    public class CopyError : ICommand
+    {
+        private readonly TextBox textBlock;
+
+        public CopyError(TextBox textBlock)
+        {
+            this.textBlock = textBlock;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (this.textBlock != null)
+            {
+                Clipboard.SetText(this.textBlock.Text);
+            }      
+        }
+
+        public event EventHandler CanExecuteChanged;
     }
 }
