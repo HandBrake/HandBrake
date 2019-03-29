e8f5368c4000cf6a07ee4ad28393ce0051fb7d22
sr55 <sr55.hb@outlook.com>
2019-03-29 21:31:49 +0000

    WinGui: Increase Queue Window size and make more space for the Filenames on the left panel.  #2009

--- win/CS/HandBrakeWPF/Properties/Resources.Designer.cs
+++ win/CS/HandBrakeWPF/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace HandBrakeWPF.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -3842,7 +3842,7 @@ namespace HandBrakeWPF.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Destination: .
+        ///   Looks up a localized string similar to Dest: .
         /// </summary>
         public static string QueueView_Destination {
             get {
@@ -4067,7 +4067,7 @@ namespace HandBrakeWPF.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Source: .
+        ///   Looks up a localized string similar to Src: .
         /// </summary>
         public static string QueueView_Source {
             get {
--- win/CS/HandBrakeWPF/Views/QueueView.xaml.cs
+++ win/CS/HandBrakeWPF/Views/QueueView.xaml.cs
@@ -39,9 +39,11 @@ namespace HandBrakeWPF.Views
             // Make the view adaptive. 
             if (e.WidthChanged)
             {
-                this.summaryTabControl.Visibility = this.ActualWidth < 600 ? Visibility.Collapsed : Visibility.Visible;
-                this.leftTabPanel.Width = this.ActualWidth < 600 ? new GridLength(this.ActualWidth - 10, GridUnitType.Star) : new GridLength(3, GridUnitType.Star);
-                this.leftTabPanel.MaxWidth = this.ActualWidth < 600 ? 650 : 400;
+                int queueSizeLimit = 675;
+
+                this.summaryTabControl.Visibility = this.ActualWidth < queueSizeLimit ? Visibility.Collapsed : Visibility.Visible;
+                this.leftTabPanel.Width = this.ActualWidth < queueSizeLimit ? new GridLength(this.ActualWidth - 10, GridUnitType.Star) : new GridLength(3, GridUnitType.Star);
+                this.leftTabPanel.MaxWidth = this.ActualWidth < queueSizeLimit ? 680 : 500;
             }
         }
         private void ContextMenu_OnOpened(object sender, RoutedEventArgs e)
