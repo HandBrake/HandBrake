aed187e1b8e8d29d23dea695f80924a6d28bce2d
sr55 <sr55.hb@outlook.com>
2019-05-22 19:15:10 +0100

    WinGui: Fix Copyright header

--- win/CS/HandBrakeWPF/Services/Scan/Factories/TitleFactory.cs
+++ win/CS/HandBrakeWPF/Services/Scan/Factories/TitleFactory.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="TitleFactory.cs" company="">
+// <copyright file="TitleFactory.cs" company="HandBrake Project (http://handbrake.fr)">
 //   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
 // </copyright>
 // <summary>
