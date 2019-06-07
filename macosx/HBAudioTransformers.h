9023a245b3b5201fc93bd5616e55c3a4e0287592
Damiano Galassi <damiog@gmail.com>
2019-06-07 12:08:00 +0200

    MacGui: localize the 'None' item in the fallback encoders popup.

--- macosx/HBAudioTransformers.h
+++ macosx/HBAudioTransformers.h
@@ -1,10 +1,8 @@
-//
-//  HBAudioTransformers.h
-//  HandBrake
-//
-//  Created by Damiano Galassi on 26/08/2016.
-//
-//
+/*  HBAudioTransformers.m $
+
+ This file is part of the HandBrake source code.
+ Homepage: <http://handbrake.fr/>.
+ It may be used under the terms of the GNU General Public License. */
 
 #import <Foundation/Foundation.h>
 
@@ -12,6 +10,12 @@
  *  A series of value trasformers to bridge the libhb enums
  *  to the textual rapresentations used in the interface.
  */
+@interface HBFallbackEncodersTransformer : NSValueTransformer
+@end
+
+@interface HBFallbackEncoderTransformer : NSValueTransformer
+@end
+
 @interface HBEncoderTransformer : NSValueTransformer
 @end
 
--- macosx/HBAudioTransformers.m
+++ macosx/HBAudioTransformers.m
@@ -1,15 +1,91 @@
-//
-//  HBAudioTransformers.m
-//  HandBrake
-//
-//  Created by Damiano Galassi on 26/08/2016.
-//
-//
+/*  HBAudioTransformers.m $
+
+ This file is part of the HandBrake source code.
+ Homepage: <http://handbrake.fr/>.
+ It may be used under the terms of the GNU General Public License. */
 
 #import "HBAudioTransformers.h"
+#import "HBLocalizationUtilities.h"
 #include "hb.h"
 
 #pragma mark - Value Trasformers
+
+@implementation HBFallbackEncodersTransformer
+
++ (Class)transformedValueClass
+{
+    return [NSArray class];
+}
+
+- (id)transformedValue:(id)value
+{
+    if (value != nil)
+    {
+        NSMutableArray *localizedArray = [NSMutableArray array];
+
+        for (NSString *text in value)
+        {
+            if ([text isEqualToString:@"None"])
+            {
+                [localizedArray addObject:HBKitLocalizedString(@"None", @"HBSubtitles -> none track name")];
+            }
+            else
+            {
+                [localizedArray addObject:text];
+            }
+        }
+        return localizedArray;
+    }
+
+    return value;
+}
+
++ (BOOL)allowsReverseTransformation
+{
+    return NO;
+}
+
+@end
+
+@implementation HBFallbackEncoderTransformer
+
++ (Class)transformedValueClass
+{
+    return [NSString class];
+}
+
+- (id)transformedValue:(id)value
+{
+    const char *name = hb_audio_encoder_get_name([value intValue]);
+    if (name)
+    {
+        return @(name);
+    }
+    else
+    {
+        return HBKitLocalizedString(@"None", @"HBSubtitles -> none track name");
+    }
+}
+
++ (BOOL)allowsReverseTransformation
+{
+    return YES;
+}
+
+- (id)reverseTransformedValue:(id)value
+{
+    if ([value isEqualTo:HBKitLocalizedString(@"None", @"HBSubtitles -> none track name")])
+    {
+        return @(hb_audio_encoder_get_from_name("none"));
+    }
+    else
+    {
+        return @(hb_audio_encoder_get_from_name([value UTF8String]));
+    }
+}
+
+@end
+
 
 @implementation HBEncoderTransformer
 
