988752d2bce5814044f0db63a952d7e3b29f7ff1
Damiano Galassi <damiog@gmail.com>
2019-07-20 10:14:44 +0200

    MacGui: add a NSString extension to create monospaced strings.

--- macosx/HBAttributedStringAdditions.h
+++ macosx/HBAttributedStringAdditions.h
@@ -4,7 +4,14 @@
  Homepage: <http://handbrake.fr/>.
  It may be used under the terms of the GNU General Public License. */
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+@interface NSString (HBAttributedStringAdditions)
+
+- (NSAttributedString *)HB_monospacedString;
+- (NSAttributedString *)HB_smallMonospacedString;
+
+@end
 
 @interface NSMutableAttributedString (HBAttributedStringAdditions)
 
--- macosx/HBAttributedStringAdditions.m
+++ macosx/HBAttributedStringAdditions.m
@@ -6,6 +6,9 @@
 
 #import "HBAttributedStringAdditions.h"
 
+static NSDictionary *_monospacedAttr = nil;
+static NSDictionary *_normalMonospacedAttr = nil;
+
 @implementation NSMutableAttributedString (HBAttributedStringAdditions)
 
 - (void)appendString:(NSString *)aString withAttributes:(NSDictionary *)aDictionary
@@ -13,6 +16,28 @@
     NSAttributedString *s = [[NSAttributedString alloc] initWithString:aString
                                                              attributes:aDictionary];
     [self appendAttributedString:s];
+}
+
+@end
+
+@implementation NSString (HBAttributedStringAdditions)
+
+- (NSAttributedString *)HB_monospacedString
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _normalMonospacedAttr = @{NSFontAttributeName: [NSFont monospacedDigitSystemFontOfSize:15 weight:NSFontWeightRegular]};
+    });
+    return [[NSAttributedString alloc] initWithString:self attributes:_normalMonospacedAttr];
+}
+
+- (NSAttributedString *)HB_smallMonospacedString
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _monospacedAttr = @{NSFontAttributeName: [NSFont monospacedDigitSystemFontOfSize:[NSFont smallSystemFontSize] weight:NSFontWeightRegular]};
+    });
+    return [[NSAttributedString alloc] initWithString:self attributes:_monospacedAttr];
 }
 
 @end
