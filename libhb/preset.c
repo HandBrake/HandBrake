f5078e5b7c71d9073918b0253b1fefcd83e6ab5a
John Stebbins <jstebbins.hb@gmail.com>
2019-09-09 15:58:09 -0700

    preset: change last ditch audio selection behavior
    Previously, it added "any" audio if the output track list was empty.
    Now only adds "any" audio if AudioLanguageList is empty.
    
    This allow e.g. "English or none" audio selections.  If the user wants
    "English or any", they can specify [English, Any] in AudioLanguageList.

--- libhb/preset.c
+++ libhb/preset.c
@@ -860,13 +860,11 @@ int hb_preset_job_add_audio(hb_handle_t *h, int title_index,
         add_audio_for_lang(list, preset, title, mux, copy_mask, fallback,
                            lang, behavior, mode, track_dict);
     }
-    // If no audios found, try "any" language option
-    // This can happen if AudioLanguageList is empty or if no audio
-    // matches the users preferred languages in AudioLanguageList
-    if (hb_value_array_len(list) <= 0)
+    // If AudioLanguageList is empty, try "any" language option
+    if (count <= 0)
     {
         add_audio_for_lang(list, preset, title, mux, copy_mask, fallback,
-                           "any", 1, mode, track_dict);
+                           "any", behavior, mode, track_dict);
     }
     hb_dict_free(&track_dict);
     return 0;
