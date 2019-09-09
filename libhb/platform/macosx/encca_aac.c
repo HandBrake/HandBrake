9408fc49d108e88efd9883a78b788fd4b4fafac9
John Stebbins <jstebbins.hb@gmail.com>
2019-09-09 15:12:54 -0700

    Fix a couple missed files

--- libhb/platform/macosx/encca_aac.c
+++ libhb/platform/macosx/encca_aac.c
@@ -7,7 +7,7 @@
    For full terms see the file COPYING file or visit http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-#include "hb.h"
+#include "handbrake.h"
 #include "audio_remap.h"
 #include <AudioToolbox/AudioToolbox.h>
 #include <CoreAudio/CoreAudio.h>
