89dbcaf6c5d02169f1f9472dc64583a0b814fdc4
John Stebbins <jstebbins.hb@gmail.com>
2019-09-11 10:06:59 -0700

    scripts: update builtin preset script
    Script now puts the built-in preset header in the new header file
    location libhb/handbrake/

--- scripts/build-presets.sh
+++ scripts/build-presets.sh
@@ -25,6 +25,6 @@ trap "rm ${JSON_TEMP} ${C_TEMP}" EXIT INT TERM
 echo 'const char hb_builtin_presets_json[] =' > "${C_TEMP}"
 "${SELF_DIR}/quotestring.py" "${JSON_TEMP}" >> "${C_TEMP}"
 echo ';' >> "${C_TEMP}"
-cp "${C_TEMP}" "${LIBHB_DIR}/preset_builtin.h"
+cp "${C_TEMP}" "${LIBHB_DIR}/handbrake/preset_builtin.h"
 
 exit 0
