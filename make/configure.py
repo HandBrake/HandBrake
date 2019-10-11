be83ac213ae7f51df2f02fd2ffda2176dc2ccce2
Bradley Sepos <bradley@bradleysepos.com>
2019-10-11 19:08:47 -0400

    make: Reduce automake required version to 1.15.0.
    We still support Ubuntu 18.04 which only has 1.15.1.

--- make/configure.py
+++ make/configure.py
@@ -1603,7 +1603,7 @@ try:
             gmake  = ToolProbe( 'GMAKE.exe',      'make',       'gmake', 'make', abort=True )
 
         autoconf   = ToolProbe( 'AUTOCONF.exe',   'autoconf',   'autoconf', abort=True, minversion=[2,69,0] )
-        automake   = ToolProbe( 'AUTOMAKE.exe',   'automake',   'automake', abort=True, minversion=[1,16,0] )
+        automake   = ToolProbe( 'AUTOMAKE.exe',   'automake',   'automake', abort=True, minversion=[1,15,0] )
         libtool    = ToolProbe( 'LIBTOOL.exe',    'libtool',    'libtool', abort=True )
         lipo       = ToolProbe( 'LIPO.exe',       'lipo',       'lipo', abort=False )
         pkgconfig  = ToolProbe( 'PKGCONFIG.exe',  'pkgconfig',  'pkg-config', abort=True, minversion=[0,29,2] )
