bc5c9618a5bf1037025b8b4633528dede070cd5f
John Stebbins <jstebbins.hb@gmail.com>
2019-11-06 13:03:32 -0800

    LinGui: fix license and po file merges

--- gtk/src/queuehandler.c
+++ gtk/src/queuehandler.c
@@ -7,8 +7,7 @@
  *
  * You may redistribute it and/or modify it under the terms of the
  * GNU General Public License, as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option)
- * any later version.
+ * Foundation; either version 2 of the License.
  *
  * queuehandler.c is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
