548fef8bb802d0995dcfa6d45373463f8ab51dea
John Stebbins <jstebbins.hb@gmail.com>
2019-11-16 10:59:43 -0800

    preset: fix import of Audio/SubTitle LanguageList "und"
    "und" used to mean "match any" language.  Now it means "match only
    undefined" language.  Forgot to translate "und" to new "any" option on
    preset import which breaks presets that have a language list with "und"
    defined in it.
    
    Note that this *will* break anyones new presets where they explicitly
    wanted "undefined", but this is a much smaller population than those
    using "any".

--- libhb/handbrake/preset_builtin.h
+++ libhb/handbrake/preset_builtin.h
@@ -8982,7 +8982,7 @@ const char hb_builtin_presets_json[] =
 "            \"x264Option\": \"\", \n"
 "            \"x264UseAdvancedOptions\": false\n"
 "        }, \n"
-"        \"VersionMajor\": 40, \n"
+"        \"VersionMajor\": 41, \n"
 "        \"VersionMicro\": 0, \n"
 "        \"VersionMinor\": 0\n"
 "    }\n"
