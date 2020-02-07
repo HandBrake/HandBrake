cffb5194ba8fc5232774a6334842f379887df566
agalin89 <artem.galin@gmail.com>
2020-02-07 23:12:50 +0000

    qsv: enable qsv decoding by default

--- win/CS/HandBrakeWPF/Services/UserSettingService.cs
+++ win/CS/HandBrakeWPF/Services/UserSettingService.cs
@@ -276,7 +276,7 @@ namespace HandBrakeWPF.Services
 
             // Video
             defaults.Add(UserSettingConstants.EnableQuickSyncEncoding, true);
-            defaults.Add(UserSettingConstants.EnableQuickSyncDecoding, false);
+            defaults.Add(UserSettingConstants.EnableQuickSyncDecoding, true);
             defaults.Add(UserSettingConstants.UseQSVDecodeForNonQSVEnc, false);
             defaults.Add(UserSettingConstants.EnableVceEncoder, true);
             defaults.Add(UserSettingConstants.EnableNvencEncoder, true);
