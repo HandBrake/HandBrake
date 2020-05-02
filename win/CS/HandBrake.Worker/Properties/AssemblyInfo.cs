af0f950668fd35a366d06c6a5d490776229b3827
sr55 <sr55.hb@outlook.com>
2020-05-02 14:45:20 +0100

    WinGui: Bump the version to 1.3.2

--- win/CS/HandBrake.Worker/Properties/AssemblyInfo.cs
+++ win/CS/HandBrake.Worker/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
