c57e68f0902a93a79907829758a1b7feed670d64
sr55 <sr55.hb@outlook.com>
2020-06-20 13:06:41 +0100

    WinGui: Fix typo #2922

--- win/CS/HandBrakeWPF/Converters/Subtitles/SubtitlesQueueDisplayConverter.cs
+++ win/CS/HandBrakeWPF/Converters/Subtitles/SubtitlesQueueDisplayConverter.cs
@@ -61,7 +61,7 @@ namespace HandBrakeWPF.Converters.Subtitles
 
                     if (track.Default)
                     {
-                        text = text + string.Format(", {0}", Resources.SummaryView_Forced);
+                        text = text + string.Format(", {0}", Resources.SummaryView_Default);
                     }
                     
                     sutitleTracks.AppendLine(text);
