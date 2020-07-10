ee48440a398290e9227b03825bf906d2f9d24a9e
Damiano Galassi <damiog@gmail.com>
2020-07-10 17:05:37 +0200

    build: add arm64-apple-darwin arch support to configure.

--- make/configure.py
+++ make/configure.py
@@ -675,6 +675,7 @@ class ArchAction( Action ):
             pass
         elif host_tuple.match( '*-*-darwin*' ):
             self.mode['x86_64'] = 'x86_64-apple-darwin%s'    % (host_tuple.release)
+            self.mode['arm64'] = 'arm64-apple-darwin%s'    % (host_tuple.release)
         elif host_tuple.match( '*-*-linux*' ):
             pass
         elif host_tuple.match( '*-*-solaris*' ):
