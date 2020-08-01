9b008b0f3666e7deb5965691e045a03d1912d6a1
Bradley Sepos <bradley@bradleysepos.com>
2020-08-01 01:50:07 -0400

    build: make: Cosmetic in configure.py.
    Doh.

--- make/configure.py
+++ make/configure.py
@@ -674,8 +674,8 @@ class ArchAction( Action ):
         elif host_tuple.match( '*-*-mingw*' ):
             pass
         elif host_tuple.match( '*-*-darwin*' ):
-            self.mode['x86_64'] = 'x86_64-apple-darwin%s' % (host_tuple.release)
-            self.mode['aarch64']  =  'aarch64-apple-darwin%s' % (host_tuple.release)
+            self.mode['x86_64']  =  'x86_64-apple-darwin%s' % (host_tuple.release)
+            self.mode['aarch64'] = 'aarch64-apple-darwin%s' % (host_tuple.release)
         elif host_tuple.match( '*-*-linux*' ):
             pass
         elif host_tuple.match( '*-*-solaris*' ):
