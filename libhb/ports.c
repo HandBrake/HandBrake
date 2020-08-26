60b8fd64c1e92cc62e8f3afcffb3cea15f06b785
Josh Soref <jsoref@users.noreply.github.com>
2020-08-26 11:24:38 -0400

    spelling: eventual
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- libhb/ports.c
+++ libhb/ports.c
@@ -647,7 +647,7 @@ char * hb_get_temporary_directory()
     else
         base = strdup("/tmp");
 #endif
-    /* I prefer to remove evntual last '/' (for cygwin) */
+    /* I prefer to remove eventual last '/' (for cygwin) */
     if( base[strlen(base)-1] == '/' )
         base[strlen(base)-1] = '\0';
 
