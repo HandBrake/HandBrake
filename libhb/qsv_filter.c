cd34e874cf8d302e59e63b55967a7fb83082bb92
Josh Soref <jsoref@users.noreply.github.com>
2020-08-26 12:10:11 -0400

    spelling: common
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- libhb/qsv_filter.c
+++ libhb/qsv_filter.c
@@ -461,7 +461,7 @@ static void hb_qsv_filter_close( hb_filter_object_t * filter )
         // closing local stuff
         qsv_filter_close(qsv,HB_QSV_VPP_DEFAULT);
 
-        // closing the commong stuff
+        // closing the common stuff
         hb_qsv_context_clean(qsv,hb_qsv_full_path_is_enabled(pv->job));
     }
     hb_buffer_list_close(&pv->list);
