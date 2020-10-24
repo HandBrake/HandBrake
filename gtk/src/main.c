04fed76bf7501080671ebb8a7579bfd203f4cf5e
Bastien Nocera <hadess@hadess.net>
2020-10-24 11:49:58 +0200

    Remove udev dependency
    In addition to udev not working correctly inside a sandbox, the icon
    names given out by the gio library (through gvfs' udisks2 volume monitor)
    are just as accurate as the udev optical disc detection, as gvfs uses
    the icon name given by udisks itself, which derives them from the device
    property udev adds:
    https://github.com/storaged-project/udisks/blob/master/udisks/udisksobjectinfo.c#L177-L196
    https://github.com/systemd/systemd/blob/master/src/udev/cdrom_id/cdrom_id.c#L914-L997

--- gtk/src/main.c
+++ gtk/src/main.c
@@ -1062,8 +1062,6 @@ ghb_activate_cb(GApplication * app, signal_user_data_t * ud)
     ghb_resource_init();
     ghb_load_icons();
 
-    ghb_udev_init();
-
     // Override user config dir
     if (arg_config_dir != NULL)
     {
