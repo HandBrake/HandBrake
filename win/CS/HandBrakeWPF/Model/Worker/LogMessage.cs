fc7458d40ee6a575c1a6257ec8f1ffb1fad59efe
sr55 <sr55.hb@outlook.com>
2021-02-20 15:35:08 +0000

    WinGui: Add required worker files for last checkin.

---
+++ win/CS/HandBrakeWPF/Model/Worker/LogMessage.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LogMessage.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   An Immutable log message
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.Model.Logging
+{
+    public class LogMessage
+    {
+        public LogMessage(string content, int messageIndex)
+        {
+            this.Content = content;
+            this.MessageIndex = messageIndex;
+        }
+
+        public string Content { get; private set; }
+
+        public int MessageIndex { get; private set; }
+    }
+}
