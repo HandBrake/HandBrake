4a4a13a0178e03171845c07bb11ad1c402e19c6e
sr55 <sr55.hb@outlook.com>
2021-09-18 15:42:49 +0100

    WinGui: Fix an issue that prevented "None" from being selected in the resolution limit field.
    (cherry picked from commit 005c8fe96cf0a4c620f28083518bf92303be81c4)

--- win/CS/HandBrakeWPF/ViewModels/PictureSettingsViewModel.cs
+++ win/CS/HandBrakeWPF/ViewModels/PictureSettingsViewModel.cs
@@ -466,9 +466,9 @@ namespace HandBrakeWPF.ViewModels
             // Set the Maintain Aspect ratio.
             this.MaintainAspectRatio = preset.Task.KeepDisplayAspect;
 
-            // Setup the Maximum Width / Height with sane 4K fallback.
-            this.MaxWidth = preset.Task.MaxWidth ?? 3840;
-            this.MaxHeight = preset.Task.MaxHeight ?? 2160;
+            // Setup the Maximum Width / Height
+            this.MaxWidth = preset.Task.MaxWidth;
+            this.MaxHeight = preset.Task.MaxHeight;
             this.SetSelectedPictureSettingsResLimitMode();
 
             // Set the width, then check the height doesn't breach the max height and correct if necessary.
@@ -883,6 +883,12 @@ namespace HandBrakeWPF.ViewModels
 
         private void SetSelectedPictureSettingsResLimitMode()
         {
+            if (this.MaxWidth == null && this.MaxHeight == null)
+            {
+                this.SelectedPictureSettingsResLimitMode = PictureSettingsResLimitModes.None;
+                return;
+            }
+
             // Look for a matching resolution.
             foreach (PictureSettingsResLimitModes limit in EnumHelper<PictureSettingsResLimitModes>.GetEnumList())
             {
