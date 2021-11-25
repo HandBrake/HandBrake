1e3234fd499ba36afad12f27b7963a0d5d634949
sr55 <sr55.hb@outlook.com>
2021-11-25 20:22:17 +0000

    WinGui:  Fix a spacing issue in translation of the status bar. Also fixed a missing translateable string (#3990).

--- win/CS/HandBrakeWPF/ViewModels/MainViewModel.cs
+++ win/CS/HandBrakeWPF/ViewModels/MainViewModel.cs
@@ -2192,7 +2192,7 @@ namespace HandBrakeWPF.ViewModels
         private void QueueProcessor_QueueJobStatusChanged(object sender, EventArgs e)
         {
             List<QueueProgressStatus> queueJobStatuses = this.queueProcessor.GetQueueProgressStatus();
-            string jobsPending = string.Format(Resources.Main_JobsPending_addon, this.queueProcessor.Count);
+            string jobsPending = "   " + string.Format(Resources.Main_JobsPending_addon, this.queueProcessor.Count);
 
             if (this.queueProcessor.IsPaused)
             {
@@ -2231,7 +2231,7 @@ namespace HandBrakeWPF.ViewModels
                     else if (queueJobStatuses.Count > 1)
                     {
                         this.windowsTaskbar.SetTaskBarProgressToNoProgress();
-                        this.ProgramStatusLabel = string.Format("{0} jobs completed. {1}Working on {2} jobs with {3} waiting to be processed.", this.queueProcessor.CompletedCount, Environment.NewLine, queueJobStatuses.Count, this.queueProcessor.Count);
+                        this.ProgramStatusLabel = string.Format(Resources.Main_QueueMultiJobStatus, this.queueProcessor.CompletedCount, Environment.NewLine, queueJobStatuses.Count, this.queueProcessor.Count);
                         this.IsMultiProcess = true;
                         this.NotifyOfPropertyChange(() => this.IsMultiProcess);
                     }
