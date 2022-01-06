f2602c5a2748e3d185d700454f8c3f06a4bd30b3
Bradley Sepos <bradley@bradleysepos.com>
2022-01-05 23:31:59 -0500

    build: Require autoconf 2.71.0 on macOS in configure.py.
    See https://github.com/HandBrake/HandBrake/commit/89118715002f42b610fcafed78a22b39a92fedd5.

--- make/configure.py
+++ make/configure.py
@@ -1650,7 +1650,7 @@ try:
         else:
             gmake  = ToolProbe( 'GMAKE.exe',      'make',       'gmake', 'make', abort=True )
 
-        autoconf   = ToolProbe( 'AUTOCONF.exe',   'autoconf',   'autoconf', abort=True, minversion=[2,69,0] )
+        autoconf   = ToolProbe( 'AUTOCONF.exe',   'autoconf',   'autoconf', abort=True, minversion=([2,71,0] if build_tuple.match('*-*-darwin*') else [2,69,0]) )
         automake   = ToolProbe( 'AUTOMAKE.exe',   'automake',   'automake', abort=True, minversion=[1,13,0] )
         libtool    = ToolProbe( 'LIBTOOL.exe',    'libtool',    'libtool', abort=True )
         lipo       = ToolProbe( 'LIPO.exe',       'lipo',       'lipo', abort=False )
