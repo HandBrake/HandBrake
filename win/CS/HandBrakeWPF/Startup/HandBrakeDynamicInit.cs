0ddc133d33360ad6371d3a9c613e048d9bce207f
sr55 <sr55.code@outlook.com>
2022-02-05 20:05:52 +0000

    WinGui: Fix an issue with the app themeing that would cause breakage with High Contrast mode on Windows. Fixes #4148
    (cherry picked from commit 6b7661427850ad78d74d393a36711255ad8077b3)

---
+++ win/CS/HandBrakeWPF/Startup/HandBrakeDynamicInit.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HandBrakeDynamic.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   Dynamically load the correct hb.dll.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.Startup
+{
+    using System.IO;
+    using System.Runtime.InteropServices;
+
+    using HandBrakeWPF.Utilities;
+
+    public static class HandBrakeDynamic
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetDllDirectory(string lpPathName);
+
+        public static void Init()
+        {
+            string dllPath = SystemInfo.IsArmDevice ? "arm64" : string.Empty;
+            string dllDir = Path.Combine(Directory.GetCurrentDirectory(), dllPath);
+            SetDllDirectory(dllDir);
+        }
+    }
+}
