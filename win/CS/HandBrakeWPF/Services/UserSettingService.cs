d2a008c6374e8c147b5e6b3f3fec7c443fe32318
galinart <artem.galin@intel.com>
2022-03-04 18:37:11 +0000

    WinGui: disable HyperEncode as default

--- win/CS/HandBrakeWPF/Services/UserSettingService.cs
+++ win/CS/HandBrakeWPF/Services/UserSettingService.cs
@@ -313,7 +313,7 @@ namespace HandBrakeWPF.Services
             // Video
             defaults.Add(UserSettingConstants.EnableQuickSyncEncoding, true);
             defaults.Add(UserSettingConstants.EnableQuickSyncDecoding, true);
-            defaults.Add(UserSettingConstants.EnableQuickSyncHyperEncode, true);
+            defaults.Add(UserSettingConstants.EnableQuickSyncHyperEncode, false);
             defaults.Add(UserSettingConstants.UseQSVDecodeForNonQSVEnc, false);
             defaults.Add(UserSettingConstants.EnableVceEncoder, true);
             defaults.Add(UserSettingConstants.EnableNvencEncoder, true);
