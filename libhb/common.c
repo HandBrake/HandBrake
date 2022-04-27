3ffb5cd5979af480acf7780fc84acff19c15ee86
FeRD (Frank Dana) <ferdnyc@gmail.com>
2022-04-27 11:51:33 -0400

    libhb: Fix hb_audio_bitrates alignment

--- libhb/common.c
+++ libhb/common.c
@@ -160,7 +160,7 @@ hb_rate_t *hb_audio_bitrates_last_item  = NULL;
 hb_rate_internal_t hb_audio_bitrates[]  =
 {
     // AC3-compatible bitrates
-    { {   "6",   6, }, NULL, 1, },
+    { {   "6",     6, }, NULL, 1, },
     { {   "12",   12, }, NULL, 1, },
     { {   "24",   24, }, NULL, 1, },
     { {   "32",   32, }, NULL, 1, },
