6075a5117d04288cfd673f0f3f093bfa7fa6cea5
sr55 <sr55.code@outlook.com>
2022-06-08 21:03:25 +0100

    WinGui: Improve window management. Any minimised windows will unminimise when activated by the user.

---
+++ win/CS/HandBrakeWPF/Helpers/WindowHelper.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WindowHelper.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// <summary>
+//   Generic Window Helper to allow spawning or re-opening windows.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrakeWPF.Helpers
+{
+    using System;
+    using System.Linq;
+    using System.Windows;
+
+    using Caliburn.Micro;
+
+    public class WindowHelper
+    {
+        public static void SpawnWindow<T>(IWindowManager windowManager, Type viewType)
+        {
+            Window window = Application.Current.Windows.Cast<Window>().FirstOrDefault(x => x.GetType() == viewType);
+
+            if (window != null)
+            {
+                if (window.WindowState == WindowState.Minimized)
+                {
+                    window.WindowState = WindowState.Normal;
+                }
+                window.Activate();
+            }
+            else
+            {
+                T logvm = IoC.Get<T>();
+                windowManager.ShowWindowAsync(logvm);
+            }
+        }
+    }
+}
