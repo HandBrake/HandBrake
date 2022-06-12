edbe6fe1ccdaf89a64d80783b877ce546f7b135f
klever1988 <56048681+klever1988@users.noreply.github.com>
2022-06-13 00:06:49 +0800

    update

--- .github/workflows/build_handbrakeCLI.sh
+++ .github/workflows/build_handbrakeCLI.sh
@@ -1,12 +1,6 @@
-yum groupinstall -y "Development Tools" "Additional Development"
-yum install -y fribidi-devel git jansson-devel libogg-devel libsamplerate-devel libtheora-devel libvorbis-devel opus-devel speex-devel xz-devel
-yum install -y epel-release
-yum install -y libass-devel yasm
-yum localinstall -y $(curl -L -s 'https://archives.fedoraproject.org/pub/archive/epel/6/x86_64/Packages/o/' | grep -Eo 'opus-[^">]+\.x86_64\.rpm' | sort -u | awk '{ print "https://archives.fedoraproject.org/pub/archive/epel/6/x86_64/Packages/o/"$0 }')
-curl -L 'https://nasm.us/nasm.repo' -o /etc/yum.repos.d/nasm.repo
-yum install -y nasm
-yum localinstall -y --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
-yum install -y lame-devel x264-devel
+#!/usr/bin/scl enable devtoolset-7 -- bash
+
+set -e
 
 export PYTHONHTTPSVERIFY=0
 
