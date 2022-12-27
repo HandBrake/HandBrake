fa577b85d5f4d9f5d5cb053e22cac1f5a794ea4a
Bradley Sepos <bradley@bradleysepos.com>
2022-12-27 14:42:38 -0500

    preset: Bump internal presets major revision number.

--- libhb/handbrake/preset_builtin.h
+++ libhb/handbrake/preset_builtin.h
@@ -10115,7 +10115,7 @@ const char hb_builtin_presets_json[] =
 "            \"x264Option\": \"\", \n"
 "            \"x264UseAdvancedOptions\": false\n"
 "        }, \n"
-"        \"VersionMajor\": 49, \n"
+"        \"VersionMajor\": 50, \n"
 "        \"VersionMicro\": 0, \n"
 "        \"VersionMinor\": 0\n"
 "    }\n"
