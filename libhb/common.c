b65d4c6515135f348adae0aa0e6330ecd7e150aa
Damiano galassi <damiog@gmail.com>
2023-01-18 10:27:45 +0100

    libhb: disable two-pass for svt-av1 encoder, it's not implemented yet.

--- libhb/common.c
+++ libhb/common.c
@@ -1551,6 +1551,8 @@ int hb_video_twopass_is_supported(uint32_t codec)
         case HB_VCODEC_QSV_H265_10BIT:
         case HB_VCODEC_QSV_AV1:
         case HB_VCODEC_QSV_AV1_10BIT:
+        case HB_VCODEC_FFMPEG_SVT_AV1:
+        case HB_VCODEC_FFMPEG_SVT_AV1_10BIT:
             return 0;
 
         default:
