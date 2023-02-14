9713aba5eb2be95be9fef55bee316e3c892ba8c0
Damiano galassi <damiog@gmail.com>
2023-02-14 08:16:44 +0100

    lang: fix the length check.

--- libhb/lang.c
+++ libhb/lang.c
@@ -314,7 +314,7 @@ int lang_to_code(const iso639_lang_t *lang)
 {
     int code = 0;
 
-    if (lang && strlen(lang->iso639_1) > 2)
+    if (lang && strlen(lang->iso639_1) >= 2)
     {
         code = (lang->iso639_1[0] << 8) | lang->iso639_1[1];
     }
