cedeacb8a2f29d05814ac1e3ff06cf5f33936f5f
sr55 <sr55.code@outlook.com>
2023-03-03 20:50:24 +0000

    WinGui: Minor UX improvement to the Add Queue window.

--- win/CS/HandBrakeWPF/Model/SelectionTitle.cs
+++ win/CS/HandBrakeWPF/Model/SelectionTitle.cs
@@ -9,6 +9,10 @@
 
 namespace HandBrakeWPF.Model
 {
+    using System;
+    using System.IO;
+
+    using HandBrakeWPF.Helpers;
     using HandBrakeWPF.Services.Scan.Model;
     using HandBrakeWPF.ViewModels;
 
@@ -83,5 +87,38 @@ namespace HandBrakeWPF.Model
         /// Gets or sets the title.
         /// </summary>
         public Title Title { get; set; }
+
+        public string SourceInfo => string.Format("{0}{1}", FilesizeStr, SourceInfoHelper.GenerateSourceInfo(this.Title));
+
+        public decimal? FilesizeMB => CalcFilesize();
+
+        public string FilesizeStr
+        {
+            get
+            {
+                if (FilesizeMB.HasValue)
+                {
+                    return string.Format("{0} MB, ", FilesizeMB);
+                }
+
+                return null;
+            }
+        }
+
+        private decimal? CalcFilesize()
+        {
+            if (this.Title != null && !string.IsNullOrEmpty(this.Title.SourceName) && File.Exists(this.Title.SourceName))
+            {
+                if (Path.GetExtension(this.Title.SourceName)?.Contains("iso", StringComparison.InvariantCultureIgnoreCase) ?? false)
+                {
+                    return null;
+                }
+
+                FileInfo info = new FileInfo(this.Title.SourceName);
+                return Math.Round((decimal)info.Length / 1024 / 1024, 1);
+            }
+
+            return null;
+        }
     }
 }
