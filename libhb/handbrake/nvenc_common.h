74dc45e33b8f77f10e7fe1c319876ba74f3e5e0c
sr55 <sr55.code@outlook.com>
2023-04-15 13:19:38 +0100

    nvenc: Improve hardware detection for NVENC so unsupported encoders are not shown. Fixes #'1666

--- libhb/handbrake/nvenc_common.h
+++ libhb/handbrake/nvenc_common.h
@@ -18,6 +18,7 @@ int            hb_nvenc_av1_available();
 int            hb_check_nvenc_available();
 int            hb_check_nvdec_available();
 
+int            hb_nvenc_get_max_instances();
 
 char*          hb_map_nvenc_preset_name (const char * preset);
 
@@ -33,5 +34,7 @@ const char*    hb_nvdec_get_codec_name(enum AVCodecID codec_id);
 int            hb_nvdec_is_enabled(hb_job_t *job);
 
 int            hb_nvdec_are_filters_supported(hb_list_t *filters);
+
+
 
 #endif // HANDBRAKE_NVENC_COMMON_H
