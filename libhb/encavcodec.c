HandBrake/HandBrake

commit 395676a92aa4ade3ee7a83157c6e3e76738ac480

libhb: avoid copying frames when converting from avframe to hb_buffer and back.

libhb/encavcodec.c
@@ -1294,14 +1294,14 @@ static void get_packets( hb_work_object_t * w, hb_buffer_list_t * list )
     }
 }
 
-static void Encode( hb_work_object_t *w, hb_buffer_t *in,
+static void Encode( hb_work_object_t *w, hb_buffer_t **buf_in,
                     hb_buffer_list_t *list )
 {
     hb_work_private_t * pv = w->private_data;
+    hb_buffer_t       * in = *buf_in;
     AVFrame             frame = {{0}};
+    int                 key_frame = 0;
     int                 ret;
-
-    hb_video_buffer_to_avframe(&frame, in);
 
     if (in->s.new_chap > 0 && pv->job->chapter_markers)
     {
@@ -1310,14 +1310,9 @@ static void Encode( hb_work_object_t *w, hb_buffer_t *in,
            currently buffered in the encoder remember the timestamp so
            when this frame finally pops out of the encoder we'll mark
            its buffer as the start of a chapter. */
-        frame.pict_type = AV_PICTURE_TYPE_I;
-        frame.key_frame = 1;
+        key_frame = 1;
         hb_chapter_enqueue(pv->chapter_queue, in);
     }
-
-    // For constant quality, setting the quality in AVCodecContext
-    // doesn't do the trick.  It must be set in the AVFrame.
-    frame.quality = pv->context->global_quality;
 
     // Bizarro ffmpeg requires timestamp time_base to be == framerate
     // for the encoders we care about.  It writes AVCodecContext.time_base
@@ -1336,7 +1331,20 @@ static void Encode( hb_work_object_t *w, hb_buffer_t *in,
     save_frame_info(pv, in);
     compute_dts_offset(pv, in);
 
+    // Convert the hb_buffer_t to avframe
+    // This will consume the hb_buffer_t and make it NULL
+    hb_video_buffer_to_avframe(&frame, buf_in);
     frame.pts = pv->frameno_in++;
+
+    // For constant quality, setting the quality in AVCodecContext
+    // doesn't do the trick.  It must be set in the AVFrame.
+    frame.quality = pv->context->global_quality;
+
+    if (key_frame)
+    {
+        frame.pict_type = AV_PICTURE_TYPE_I;
+        frame.key_frame = 1;
+    }
 
     // Encode
     ret = avcodec_send_frame(pv->context, &frame);
@@ -1402,7 +1410,7 @@ int encavcodecWork( hb_work_object_t * w, hb_buffer_t ** buf_in,
         return HB_WORK_DONE;
     }
 
-    Encode(w, in, &list);
+    Encode(w, buf_in, &list);
     *buf_out = hb_buffer_list_clear(&list);
 
     return HB_WORK_OK;

libhb/avfilter.c
@@ -232,7 +232,7 @@ void hb_avfilter_alias_close( hb_filter_object_t * filter )
     filter->private_data = NULL;
 }
 
-static hb_buffer_t* filterFrame( hb_filter_private_t * pv, hb_buffer_t * in )
+static hb_buffer_t* filterFrame( hb_filter_private_t * pv, hb_buffer_t ** buf_in )
 {
     hb_buffer_list_t   list;
     hb_buffer_t      * buf = NULL, * next = NULL;
@@ -240,8 +240,9 @@ static hb_buffer_t* filterFrame( hb_filter_private_t * pv, hb_buffer_t * in )
 #if HB_PROJECT_FEATURE_QSV && (defined( _WIN32 ) || defined( __MINGW32__ ))
     mfxFrameSurface1 *surface = NULL;
     HBQSVFramesContext *frames_ctx = NULL;
-    if (hb_qsv_hw_filters_are_enabled(pv->input.job))
+    if (hb_qsv_hw_filters_are_enabled(pv->input.job) && buf_in != NULL)
     {
+        hb_buffer_t      *in = *buf_in;
         if (in && in->qsv_details.frame)
         {
             // We need to keep surface pointer because hb_avfilter_add_buf set it to 0 after in ffmpeg call
@@ -256,7 +257,7 @@ static hb_buffer_t* filterFrame( hb_filter_private_t * pv, hb_buffer_t * in )
 #endif
     if (av_frame_is_not_null)
     {
-        hb_avfilter_add_buf(pv->graph, in);
+        hb_avfilter_add_buf(pv->graph, buf_in);
         buf = hb_avfilter_get_buf(pv->graph);
     }
     while (buf != NULL)
@@ -305,7 +306,7 @@ static int avfilter_work( hb_filter_object_t * filter,
         return HB_FILTER_DONE;
     }
 
-    *buf_out = filterFrame(pv, in);
+    *buf_out = filterFrame(pv, buf_in);
 
     return HB_FILTER_OK;
 }

libhb/chroma_smooth.c
@@ -88,7 +88,8 @@ static void name##_##nbits(const uint8_t *frame_src,                            
                                  uint8_t *frame_dst,                                                        \
                            const int width,                                                                 \
                            const int height,                                                                \
-                           int stride,                                                                      \
+                           int stride_src,                                                                  \
+                           int stride_dst,                                                                  \
                            chroma_smooth_plane_context_t * ctx,                                             \
                            chroma_smooth_thread_context_t * tctx)                                           \
 {                                                                                                           \
@@ -112,7 +113,20 @@ static void name##_##nbits(const uint8_t *frame_src,                            
     {                                                                                                       \
         if (src != dst)                                                                                     \
         {                                                                                                   \
-            memcpy(dst, src, stride*height);                                                                \
+            if (stride_src == stride_dst)                                                                   \
+            {                                                                                               \
+                memcpy(dst, src, stride_dst * height);                                                      \
+            }                                                                                               \
+            else                                                                                            \
+            {                                                                                               \
+                const int size = stride_src < stride_dst ? ABS(stride_src) : stride_dst;                    \
+                for (int yy = 0; yy < height; yy++)                                                         \
+                {                                                                                           \
+                    memcpy(dst, src, size);                                                                 \
+                    dst += stride_dst;                                                                      \
+                    src += stride_src;                                                                      \
+                }                                                                                           \
+            }                                                                                               \
         }                                                                                                   \
                                                                                                             \
         return;                                                                                             \
@@ -123,7 +137,8 @@ static void name##_##nbits(const uint8_t *frame_src,                            
         memset(SC[y], 0, sizeof(SC[y][0]) * (width + 2 * steps));                                           \
     }                                                                                                       \
                                                                                                             \
-    stride /= ctx->bps;                                                                                     \
+    stride_src /= ctx->bps;                                                                                 \
+    stride_dst /= ctx->bps;                                                                                 \
                                                                                                             \
     for (y = -steps; y < height + steps; y++)                                                               \
     {                                                                                                       \
@@ -152,8 +167,8 @@ static void name##_##nbits(const uint8_t *frame_src,                            
                                                                                                             \
             if (x >= steps && y >= steps)                                                                   \
             {                                                                                               \
-                const uint##nbits##_t *srx = src - steps * stride + x - steps;                              \
-                uint##nbits##_t       *dsx = dst - steps * stride + x - steps;                              \
+                const uint##nbits##_t *srx = src - steps * stride_src + x - steps;                          \
+                uint##nbits##_t       *dsx = dst - steps * stride_dst + x - steps;                          \
                                                                                                             \
                 res = (int32_t)*srx - ((((int32_t)*srx -                                                    \
                       (int32_t)((Tmp1 + halfscale) >> scalebits)) * amount) >> 16);                         \
@@ -163,8 +178,8 @@ static void name##_##nbits(const uint8_t *frame_src,                            
                                                                                                             \
         if (y >= 0)                                                                                         \
         {                                                                                                   \
-            dst += stride;                                                                                  \
-            src += stride;                                                                                  \
+            dst += stride_dst;                                                                              \
+            src += stride_src;                                                                              \
         }                                                                                                   \
     }                                                                                                       \
 }                                                                                                           \
@@ -386,6 +401,7 @@ static int chroma_smooth_work_thread(hb_filter_object_t *filter,
                       in->plane[c].width,
                       in->plane[c].height,
                       in->plane[c].stride,
+                      out->plane[c].stride,
                       ctx, tctx);
     }
 

libhb/decavcodec.c
@@ -1158,7 +1158,7 @@ static hb_buffer_t *copy_frame( hb_work_private_t *pv )
     else
 #endif
     {
-        out = hb_avframe_to_video_buffer(pv->frame, (AVRational){1,1});
+        out = hb_avframe_to_video_buffer(pv->frame, (AVRational){1,1}, 1);
     }
 
     if (pv->frame->pts != AV_NOPTS_VALUE)

libhb/decomb.c
@@ -573,6 +573,15 @@ static int hb_decomb_work(hb_filter_object_t *filter,
     hb_filter_private_t *pv = filter->private_data;
     hb_buffer_t *in = *buf_in;
 
+    // TODO: eliminate extra buffer copies in decomb
+    if (in->plane[0].height_stride - in->plane[0].height < 3)
+    {
+        // Decomb requires some additional rows
+        // to work on.
+        in = hb_buffer_dup(in);
+        hb_buffer_close(buf_in);
+    }
+
     // Input buffer is always consumed.
     *buf_in = NULL;
     if (in->s.flags & HB_BUF_FLAG_EOF)

libhb/detelecine.c
@@ -1044,9 +1044,30 @@ static int hb_detelecine_work( hb_filter_object_t * filter,
     }
 
     /* Copy input buffer into pullup buffer */
-    memcpy( buf->planes[0], in->plane[0].data, buf->size[0] );
-    memcpy( buf->planes[1], in->plane[1].data, buf->size[1] );
-    memcpy( buf->planes[2], in->plane[2].data, buf->size[2] );
+    /* Copy pullup frame buffer into output buffer */
+    for (int pp = 0; pp < 3; pp++)
+    {
+        if (in->plane[pp].stride == ctx->stride[pp])
+        {
+            memcpy(buf->planes[pp], in->plane[pp].data, buf->size[pp]);
+        }
+        else
+        {
+            const int stride_src = in->plane[pp].stride;
+            const int stride_dst = ctx->stride[pp];
+            const int height = in->f.height;
+            const int size = stride_src < stride_dst ? ABS(stride_src) : stride_dst;
+            uint8_t *dst = buf->planes[pp];
+            uint8_t *src = in->plane[pp].data;
+
+            for (int yy = 0; yy < height; yy++)
+            {
+                memcpy(dst, src, size);
+                dst += stride_dst;
+                src += stride_src;
+            }
+        }
+    }
 
     /* Submit buffer fields based on buffer flags.
        Detelecine assumes BFF when the TFF flag isn't present. */

libhb/fifo.c
@@ -598,70 +598,118 @@ void hb_buffer_copy_props(hb_buffer_t *dst, const hb_buffer_t *src)
     hb_buffer_copy_side_data(dst, src);
 }
 
-hb_buffer_t * hb_buffer_dup( const hb_buffer_t * src )
+static int copy_hwframe_to_video_buffer(const AVFrame *frame, hb_buffer_t *buf)
 {
+    int ret;
+    AVFrame *hw_frame = av_frame_alloc();
 
-    hb_buffer_t * buf;
-
-    if ( src == NULL )
-        return NULL;
-
-    buf = hb_buffer_init( src->size );
-    if ( buf )
+    ret = av_frame_copy_props(hw_frame, frame);
+    if (ret < 0)
     {
-        buf->f = src->f;
-        hb_buffer_copy_props(buf, src);
+        hb_log("fifo: av_frame_copy_props");
+    }
+    ret = av_hwframe_get_buffer(frame->hw_frames_ctx, hw_frame, 0);
+    if (ret < 0)
+    {
+        hb_log("fifo: av_hwframe_get_buffer failed");
+    }
+    ret = av_hwframe_transfer_data(hw_frame, frame, 0);
+    if (ret < 0)
+    {
+        hb_log("fifo: av_hwframe_transfer_data failed");
+    }
 
-        if (buf->s.type == FRAME_BUF)
+    buf->storage = hw_frame;
+    buf->storage_type = AVFRAME;
+
+    return ret;
+}
+
+static void copy_avframe_to_video_buffer(const AVFrame *frame, hb_buffer_t *buf)
+{
+    for (int pp = 0; pp <= buf->f.max_plane; pp++)
+    {
+        if (buf->plane[pp].stride == frame->linesize[pp])
         {
-            hb_buffer_init_planes(buf);
-        }
-
-        if (src->storage_type == AVFRAME)
-        {
-            AVBufferRef *hw_frames_ctx = ((AVFrame *)src->storage)->hw_frames_ctx;
-            if (hw_frames_ctx)
-            {
-                AVFrame *hw_frame = av_frame_alloc();
-
-                int ret;
-
-                ret = av_frame_copy_props(hw_frame, (AVFrame *)src->storage);
-                if (ret < 0)
-                {
-                    hb_log("fifo: av_frame_copy_props");
-                }
-                ret = av_hwframe_get_buffer(hw_frames_ctx, hw_frame, 0);
-                if (ret < 0)
-                {
-                    hb_log("fifo: av_hwframe_get_buffer failed");
-                }
-                ret = av_hwframe_transfer_data(hw_frame, (AVFrame *)src->storage, 0);
-                if (ret < 0)
-                {
-                    hb_log("fifo: av_hwframe_transfer_data failed");
-                }
-
-                buf->storage = hw_frame;
-                buf->storage_type = AVFRAME;
-            }
-            else
-            {
-                for (int pp = 0; pp <= src->f.max_plane; pp++)
-                {
-                    memcpy(buf->plane[pp].data, src->plane[pp].data, src->plane[pp].size);
-                }
-            }
+            memcpy(buf->plane[pp].data, frame->data[pp], frame->linesize[pp] * buf->plane[pp].height);
         }
         else
         {
-            memcpy( buf->data, src->data, src->size );
+            const int width     = buf->plane[pp].width;
+            const int height    = buf->plane[pp].height;
+            const int stride    = buf->plane[pp].stride;
+            const int linesize  = frame->linesize[pp];
+            uint8_t *dst = buf->plane[pp].data;
+            uint8_t *src = frame->data[pp];
+            for (int yy = 0; yy < height; yy++)
+            {
+                memcpy(dst, src, width);
+                dst += stride;
+                src += linesize;
+            }
         }
+    }
+}
 
+hb_buffer_t * hb_buffer_dup(const hb_buffer_t *src)
+{
+    hb_buffer_t *buf = NULL;
+
+    if (src == NULL)
+    {
+        return NULL;
+    }
+
+    if (src->storage_type == STANDARD)
+    {
+        buf = hb_buffer_init(src->size);
+        if (buf)
+        {
+            buf->f = src->f;
+            hb_buffer_copy_props(buf, src);
+
+            if (buf->s.type == FRAME_BUF)
+            {
+                hb_buffer_init_planes(buf);
+            }
+
+            memcpy(buf->data, src->data, src->size);
+        }
+    }
+    else if (src->storage_type == AVFRAME)
+    {
+        const AVFrame *frame = (AVFrame *)src->storage;
+
+        // If it's an hardware frame, make a copy
+        // into another hardware AVFrame.
+        if (frame->hw_frames_ctx)
+        {
+            buf = hb_buffer_wrapper_init();
+            if (buf)
+            {
+                buf->f = src->f;
+                hb_buffer_copy_props(buf, src);
+                copy_hwframe_to_video_buffer(frame, buf);
+            }
+        }
+        // If not, copy the content to a standard hb_buffer
+        else
+        {
+            buf = hb_frame_buffer_init(src->f.fmt, src->f.width, src->f.height);
+            if (buf)
+            {
+                buf->f = src->f;
+                hb_buffer_copy_props(buf, src);
+                copy_avframe_to_video_buffer(frame, buf);
+            }
+        }
     }
 
 #if HB_PROJECT_FEATURE_QSV
-    memcpy(&buf->qsv_details, &src->qsv_details, sizeof(src->qsv_details));
+    if (buf)
+    {
+        memcpy(&buf->qsv_details, &src->qsv_details, sizeof(src->qsv_details));
+    }
 #endif
 
     return buf;

libhb/handbrake/hbavfilter.h
@@ -30,7 +30,7 @@ int     hb_avfilter_add_frame(hb_avfilter_graph_t * graph, AVFrame * frame);
 
 int     hb_avfilter_get_frame(hb_avfilter_graph_t * graph, AVFrame * frame);
 
-int     hb_avfilter_add_buf(hb_avfilter_graph_t * graph, hb_buffer_t * in);
+int     hb_avfilter_add_buf(hb_avfilter_graph_t * graph, hb_buffer_t ** in);
 
 hb_buffer_t *
 hb_avfilter_get_buf(hb_avfilter_graph_t * graph);

commit f3d793ac8cdf7ca8f154f157fa23530781c7ee2a

libhb: do not pass the source frame pict_type and duration to libavcodec encoders. Fix #5530, regression introduced in 395676a.

libhb/encavcodec.c
@@ -1068,6 +1068,7 @@ static void Encode( hb_work_object_t *w, hb_buffer_t **buf_in,
     // This will consume the hb_buffer_t and make it NULL
     hb_video_buffer_to_avframe(&frame, buf_in);
     frame.pts = pv->frameno_in++;
+    frame.duration = 0;
 
     // For constant quality, setting the quality in AVCodecContext
     // doesn't do the trick.  It must be set in the AVFrame.
@@ -1076,7 +1077,12 @@ static void Encode( hb_work_object_t *w, hb_buffer_t **buf_in,
     if (key_frame)
     {
         frame.pict_type = AV_PICTURE_TYPE_I;
-        frame.flags |= AV_FRAME_FLAG_KEY;
+        frame.flags = AV_FRAME_FLAG_KEY;
+    }
+    else
+    {
+        frame.pict_type = AV_PICTURE_TYPE_NONE;
+        frame.flags = 0;
     }
 
     // Encode