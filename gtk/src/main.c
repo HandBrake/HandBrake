4fe94ebab492c6c2e8611eabc738b7d0f7878802
robxnano <89391914+robxnano@users.noreply.github.com>
2024-01-09 17:17:20 +0000

    LinGUI: Add GhbApplication class (part 2)
    Create new main.c as a stub entry point. This commit is split
    into two parts in order to keep the history of application.c.

---
+++ gtk/src/main.c
@@ -0,0 +1,43 @@
+/* main.c
+ *
+ * Copyright (C) 2008-2024 John Stebbins <stebbins@stebbins>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+#include <glib/gi18n.h>
+#include "config.h"
+#include "application.h"
+#include "settings.h"
+#include "ui_res.h"
+
+int
+main (int argc, char *argv[])
+{
+#if defined(_WIN32)
+    // Tell gdk pixbuf where it's loader config file is.
+    _putenv_s("GDK_PIXBUF_MODULE_FILE", "ghb.exe.local/loaders.cache");
+    _putenv_s("GST_PLUGIN_PATH", "lib/gstreamer-1.0");
+#endif
+
+    bindtextdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+    bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+    textdomain(GETTEXT_PACKAGE);
+
+    ghb_ui_register_resource();
+    g_autofree char *app_cmd = (argc > 0) ? g_strdup(argv[0]) : NULL;
+    g_autoptr(GApplication) app = G_APPLICATION(ghb_application_new(app_cmd));
+    return g_application_run(app, argc, argv);
+}
