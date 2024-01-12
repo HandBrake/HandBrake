765c7071f50bdc79266fd1aaf671c6125a760dfd
sr55 <sr55.code@outlook.com>
2024-01-12 22:34:14 +0000

    WinGui: Minor improvement to keyboard navigation in the chapters view. #5679
    (cherry picked from commit d9121ea9b266fb1be0ecc2b763e8979ca05eba5e)

--- win/CS/HandBrakeWPF/Views/ChaptersView.xaml.cs
+++ win/CS/HandBrakeWPF/Views/ChaptersView.xaml.cs
@@ -10,6 +10,14 @@
 namespace HandBrakeWPF.Views
 {
     using System.Windows.Controls;
+    using System.Windows.Input;
+
+    using HandBrakeWPF.Helpers;
+    using HandBrakeWPF.Services.Encode.Model.Models;
+
+    using KeyEventArgs = System.Windows.Input.KeyEventArgs;
+    using TextBox = System.Windows.Controls.TextBox;
+    using UserControl = System.Windows.Controls.UserControl;
 
     /// <summary>
     /// Interaction logic for ChaptersView.xaml
@@ -22,6 +30,31 @@ namespace HandBrakeWPF.Views
         public ChaptersView()
         {
             InitializeComponent();
+        }
+
+        private void ChaptersList_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                this.chaptersList.Focus();
+                this.chaptersList.SelectedIndex += 1; // increase
+
+                this.chaptersList.ScrollIntoView(this.chaptersList.SelectedItem);
+            }
+        }
+        
+        private void ChapterName_OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            if (tb != null)
+            {
+                ChapterMarker c = tb.DataContext as ChapterMarker;
+                ListView lv = VisualTreeUtils.FindAncestor<ListView>(tb);
+                if (lv != null && c != null)
+                {
+                    lv.SelectedItem = c;
+                }
+            }
         }
     }
 }
