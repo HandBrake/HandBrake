HandBrake/HandBrake

commit c72bb7dfa8ff86e45f15a8c103a5d8fd992c0177

libhb: remove emptry tx3g subtitles
We don't need the empty subtitle buffers.


git-svn-id: svn://svn.handbrake.fr/HandBrake/trunk@6373 b64f7644-9d1e-0410-96f1-a4d463321fa5

libhb/dectx3gsub.c
@@ -77,7 +77,7 @@ static int write_ssa_markup(char *dst, StyleRecord *style)
     return strlen(dst);
 }
 
-static hb_buffer_t *tx3g_decode_to_ssa(hb_buffer_t *in, int line)
+static hb_buffer_t *tx3g_decode_to_ssa(hb_work_private_t *pv, hb_buffer_t *in)
 {
     uint8_t *pos = in->data;
     uint8_t *end = in->data + in->size;
@@ -158,11 +158,13 @@ static hb_buffer_t *tx3g_decode_to_ssa(hb_buffer_t *in, int line)
     if ( out == NULL )
         goto fail;
     uint8_t *dst = out->data;
+    uint8_t *start;
     int charIndex = 0;
     int styleIndex = 0;
 
-    sprintf((char*)dst, "%d,,Default,,0,0,0,,", line);
+    sprintf((char*)dst, "%d,,Default,,0,0,0,,", pv->line);
     dst += strlen((char*)dst);
+    start = dst;
     for (pos = text, end = text + textLength; pos < end; pos++)
     {
         if (IS_10xxxxxx(*pos))
@@ -199,6 +201,13 @@ static hb_buffer_t *tx3g_decode_to_ssa(hb_buffer_t *in, int line)
             WRITE_CHAR(*pos);
         }
         charIndex++;
+    }
+    if (start == dst)
+    {
+        // No text in the subtitle.  This sub is just filler, drop it.
+        free(styleRecords);
+        hb_buffer_close(&out);
+        return NULL;
     }
     *dst = '\0';
     dst++;
@@ -263,7 +272,7 @@ static int dectx3gWork( hb_work_object_t * w, hb_buffer_t ** buf_in,
         return HB_WORK_DONE;
     }
 
-    *buf_out = tx3g_decode_to_ssa(in, ++pv->line);
+    *buf_out = tx3g_decode_to_ssa(pv, in);
 
     return HB_WORK_OK;
 }

commit 699c880ff6f7e48aa4cd040aba8adc0fb4b4a6ce

dectx3gsub: properly increment the line count, each SSA line in MKV (and in libass) requires an unique ReadOrder number. Fix a regression introduced in c72bb7d that was breaking burned subtitles and the creation of incorrect MKV files when the source subtitles were in the tx3g format.

libhb/dectx3gsub.c
@@ -169,7 +169,7 @@ static hb_buffer_t *tx3g_decode_to_ssa(hb_work_private_t *pv, hb_buffer_t *in)
     int charIndex = 0;
     int styleIndex = 0;
 
-    snprintf((char*)dst, maxOutputSize, "%d,,Default,,0,0,0,,", pv->line);
+    snprintf((char*)dst, maxOutputSize, "%d,0,Default,,0,0,0,,", pv->line);
     dst += strlen((char*)dst);
     start = dst;
     for (pos = text, end = text + textLength; pos < end; pos++)
@@ -218,6 +218,8 @@ static hb_buffer_t *tx3g_decode_to_ssa(hb_work_private_t *pv, hb_buffer_t *in)
     }
     *dst = '\0';
     dst++;
+
+    pv->line++;
 
     // Trim output buffer to the actual amount of data written
     out->size = dst - out->data;