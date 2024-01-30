7cb368f054170607dd3f6c1925ea65c22ad326b3
sr55 <sr55.code@outlook.com>
2024-01-30 21:16:06 +0000

    WinGui: Further Development on the new Subtitle Rules Panel

---
+++ win/CS/HandBrake.Interop/Interop/Json/Presets/SubtitleList.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SubtitleList.cs" company="HandBrake Project (http://handbrake.fr)">
+//   This file is part of the HandBrake source code - It may be used under the terms of the GNU General Public License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HandBrake.Interop.Interop.Json.Presets
+{
+    public class SubtitleList
+    {
+        public string IsoLangCode { get; set; }
+        public int TrackSelectionMode { get; set; }
+        public int DefaultMode { get; set; }
+        public int BurnPassthruMode { get; set; }
+        public int ForcedMode { get; set; }
+        public string TrackNameOverride { get; set; }
+    }
+}
