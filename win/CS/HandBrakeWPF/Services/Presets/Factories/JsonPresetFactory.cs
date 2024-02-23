f00cbb46b3045a4811a3654f59ab6b33c0d402ee
sr55 <sr55.code@outlook.com>
2024-02-23 20:59:52 +0000

    WinGui: Add Foreign Audio Scan Support back in.

--- win/CS/HandBrakeWPF/Services/Presets/Factories/JsonPresetFactory.cs
+++ win/CS/HandBrakeWPF/Services/Presets/Factories/JsonPresetFactory.cs
@@ -432,7 +432,8 @@ namespace HandBrakeWPF.Services.Presets.Factories
                     newTrack.BurnPassthruMode = (SubtitleBurnInBehaviourModes)importedTrack.BurnPassthruMode;
                     newTrack.ForcedMode = (ForcedModes)importedTrack.ForcedMode;
                     newTrack.TrackName = importedTrack.TrackNameOverride;
-                    
+                    newTrack.IsForeignAudioScanRule = importedTrack.IsForeignAudioScanRule;
+
                     preset.SubtitleTrackBehaviours.Tracks.Add(newTrack);
                 }
             }
@@ -557,6 +558,7 @@ namespace HandBrakeWPF.Services.Presets.Factories
                     subRecord.DefaultMode = (int)item.DefaultMode;
                     subRecord.ForcedMode = (int)item.ForcedMode;
                     subRecord.TrackNameOverride = item.TrackName;
+                    subRecord.IsForeignAudioScanRule = item.IsForeignAudioScanRule;
 
                     preset.SubtitleBehaviours.Add(subRecord);
                 }
