51d7ee8540519097b7321210358855954f671d7b
Damiano Galassi <damiog@gmail.com>
2024-03-27 19:30:39 +0100

    dovi_common: move the levels array out of the header file.

--- libhb/dovi_common.c
+++ libhb/dovi_common.c
@@ -11,6 +11,32 @@
 #include "handbrake/dovi_common.h"
 #include "handbrake/h265_common.h"
 
+static struct
+{
+    const uint32_t id;
+    const uint32_t max_pps;
+    const uint32_t max_width;
+    const uint32_t max_bitrate_main_tier;
+    const uint32_t max_bitrate_high_tier;
+}
+hb_dovi_levels[] =
+{
+    { 1,  22118400,   1280, 20,  50  },
+    { 2,  27648000,   1280, 20,  50  },
+    { 3,  49766400,   1920, 20,  70  },
+    { 4,  62208000,   2560, 20,  70  },
+    { 5,  124416000,  3840, 20,  70  },
+    { 6,  199065600,  3840, 25,  130 },
+    { 7,  248832000,  3840, 25,  130 },
+    { 8,  398131200,  3840, 40,  130 },
+    { 9,  497664000,  3840, 40,  130 },
+    { 10, 995328000,  3840, 60,  240 },
+    { 11, 995328000,  7680, 60,  240 },
+    { 12, 1990656000, 7680, 120, 480 },
+    { 13, 3981312000, 7680, 240, 800 },
+    { 0, 0, 0, 0, 0 }
+};
+
 int hb_dovi_max_rate(int width, int pps, int bitrate, int level, int high_tier)
 {
     int max_rate = 0;
@@ -29,14 +55,14 @@ int hb_dovi_max_rate(int width, int pps, int bitrate, int level, int high_tier)
     }
     else
     {
-        for (int i = 0; hb_dolby_vision_levels[i].id != 0; i++)
+        for (int i = 0; hb_dovi_levels[i].id != 0; i++)
         {
             int level_max_rate = high_tier ?
-                                    hb_dolby_vision_levels[i].max_bitrate_high_tier :
-                                    hb_dolby_vision_levels[i].max_bitrate_main_tier;
+                                    hb_dovi_levels[i].max_bitrate_high_tier :
+                                    hb_dovi_levels[i].max_bitrate_main_tier;
 
-            if (pps <= hb_dolby_vision_levels[i].max_pps &&
-                width <= hb_dolby_vision_levels[i].max_width &&
+            if (pps <= hb_dovi_levels[i].max_pps &&
+                width <= hb_dovi_levels[i].max_width &&
                 bitrate <= level_max_rate * 1000)
             {
                 max_rate = level_max_rate * 1000;
@@ -50,22 +76,22 @@ int hb_dovi_max_rate(int width, int pps, int bitrate, int level, int high_tier)
 
 int hb_dovi_level(int width, int pps, int max_rate, int high_tier)
 {
-    int dv_level = hb_dolby_vision_levels[12].id;
+    int dv_level = hb_dovi_levels[12].id;
 ;
 
-    for (int i = 0; hb_dolby_vision_levels[i].id != 0; i++)
+    for (int i = 0; hb_dovi_levels[i].id != 0; i++)
     {
-        int max_pps = hb_dolby_vision_levels[i].max_pps;
-        int max_width = hb_dolby_vision_levels[i].max_width;
+        int max_pps = hb_dovi_levels[i].max_pps;
+        int max_width = hb_dovi_levels[i].max_width;
         int tier_max_rate = high_tier ?
-                                hb_dolby_vision_levels[i].max_bitrate_high_tier :
-                                hb_dolby_vision_levels[i].max_bitrate_main_tier;
+                                hb_dovi_levels[i].max_bitrate_high_tier :
+                                hb_dovi_levels[i].max_bitrate_main_tier;
 
         tier_max_rate *= 1000;
 
         if (pps <= max_pps && max_rate <= tier_max_rate && width <= max_width)
         {
-            dv_level = hb_dolby_vision_levels[i].id;
+            dv_level = hb_dovi_levels[i].id;
             break;
         }
     }
--- libhb/handbrake/dovi_common.h
+++ libhb/handbrake/dovi_common.h
@@ -10,34 +10,7 @@
 #ifndef HANDBRAKE_DOVI_COMMON_H
 #define HANDBRAKE_DOVI_COMMON_H
 
-#include "handbrake/project.h"
 #include <stdint.h>
-
-static struct
-{
-    const uint32_t id;
-    const uint32_t max_pps;
-    const uint32_t max_width;
-    const uint32_t max_bitrate_main_tier;
-    const uint32_t max_bitrate_high_tier;
-}
-hb_dolby_vision_levels[] =
-{
-    { 1,  22118400,   1280, 20,  50  },
-    { 2,  27648000,   1280, 20,  50  },
-    { 3,  49766400,   1920, 20,  70  },
-    { 4,  62208000,   2560, 20,  70  },
-    { 5,  124416000,  3840, 20,  70  },
-    { 6,  199065600,  3840, 25,  130 },
-    { 7,  248832000,  3840, 25,  130 },
-    { 8,  398131200,  3840, 40,  130 },
-    { 9,  497664000,  3840, 40,  130 },
-    { 10, 995328000,  3840, 60,  240 },
-    { 11, 995328000,  7680, 60,  240 },
-    { 12, 1990656000, 7680, 120, 480 },
-    { 13, 3981312000, 7680, 240, 800 },
-    { 0, 0, 0, 0, 0 }
-};
 
 int hb_dovi_max_rate(int width, int pps, int bitrate, int level, int high_tier);
 int hb_dovi_level(int width, int pps, int max_rate, int high_tier);
