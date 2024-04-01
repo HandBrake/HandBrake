5f1af001e0b72bded9bde88c99bc60389dc9bf70
Sam H <samhutchins@users.noreply.github.com>
2024-04-01 11:19:57 +0100

    libhb: bump preset version, and disable multi-pass for existing CQ VP9 presets
     - We don't want people's existing presets to dramatically change in behaviour

--- libhb/handbrake/preset_builtin.h
+++ libhb/handbrake/preset_builtin.h
@@ -10009,7 +10009,7 @@ const char hb_builtin_presets_json[] =
 "            \"x264Option\": \"\",\n"
 "            \"x264UseAdvancedOptions\": false\n"
 "        },\n"
-"        \"VersionMajor\": 53,\n"
+"        \"VersionMajor\": 54,\n"
 "        \"VersionMicro\": 0,\n"
 "        \"VersionMinor\": 0\n"
 "    }\n"
