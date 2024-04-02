940aba31888bb759ad9780961322458fc431e03f
Damiano Galassi <damiog@gmail.com>
2024-04-02 16:10:55 +0200

    WinGui: allow multipass for CQ when available.

--- win/CS/HandBrake.Interop/Interop/Interfaces/Model/Encoders/HBVideoEncoder.cs
+++ win/CS/HandBrake.Interop/Interop/Interfaces/Model/Encoders/HBVideoEncoder.cs
@@ -148,6 +148,8 @@ namespace HandBrake.Interop.Interop.Interfaces.Model.Encoders
 
         public bool SupportsMultiPass => HandBrakeEncoderHelpers.VideoEncoderSupportsMultiPass(this.ShortName);
 
+        public bool SupportsCQMultiPass => HandBrakeEncoderHelpers.VideoEncoderSupportsCQMultiPass(this.ShortName);
+
         // TODO check if there is a nicer way of doing this.
         public bool IsSVTAV1 => this.ShortName.Contains("svt_av1");
         public bool IsX264 => this.ShortName.Contains("x264");
