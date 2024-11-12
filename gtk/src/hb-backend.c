7c0b6d3abe432cf2d3a930b4b1463235f646fa02
Sam H <samhutchins@users.noreply.github.com>
2024-11-12 20:57:44 +0000

    LinGui: Add support for Max Duration
     - Wire up the Linux GUI
     - Swap label and spinner, so the checkbox on max duration makes sense

--- gtk/src/hb-backend.c
+++ gtk/src/hb-backend.c
@@ -3545,11 +3545,11 @@ get_path_list(GListModel *files)
 }
 
 void
-ghb_backend_scan_list (GListModel *files, int titleindex, int preview_count, uint64_t min_duration, gboolean keep_duplicate_titles)
+ghb_backend_scan_list (GListModel *files, int titleindex, int preview_count, uint64_t min_duration, uint64_t max_duration, gboolean keep_duplicate_titles)
 {
     hb_list_t *path_list = get_path_list(files);
     hb_list_t *extensions = ghb_get_excluded_extensions_list();
-    hb_scan(h_scan, path_list, titleindex, preview_count, 1, min_duration, 0,
+    hb_scan(h_scan, path_list, titleindex, preview_count, 1, min_duration, max_duration,
                  0, 0, extensions, 0, keep_duplicate_titles);
     ghb_free_list(path_list);
     ghb_free_list(extensions);
@@ -3564,12 +3564,12 @@ ghb_backend_scan_list (GListModel *files, int titleindex, int preview_count, uin
 }
 
 void
-ghb_backend_scan (const char *path, int titleindex, int preview_count, uint64_t min_duration, gboolean keep_duplicate_titles)
+ghb_backend_scan (const char *path, int titleindex, int preview_count, uint64_t min_duration, uint64_t max_duration, gboolean keep_duplicate_titles)
 {
     hb_list_t *path_list = hb_list_init();
     hb_list_add(path_list, (void *)path);
     hb_list_t *extensions = ghb_get_excluded_extensions_list();
-    hb_scan(h_scan, path_list, titleindex, preview_count, 1, min_duration, 0,
+    hb_scan(h_scan, path_list, titleindex, preview_count, 1, min_duration, max_duration,
                  0, 0, extensions, 0, keep_duplicate_titles);
     hb_list_close(&path_list);
     ghb_free_list(extensions);
