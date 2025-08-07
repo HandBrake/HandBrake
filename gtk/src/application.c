9b2eaa9b98b0395a8b761c85fb46f10abc3fe478
robxnano <89391914+robxnano@users.noreply.github.com>
2025-08-07 21:18:31 +0100

    LinGUI: Support relative locale dir
    Instead of using a hardcoded location for gettext message catalogs,
    determine the location relative to the binary install dir.
    Alternatively, the TEXTDOMAINDIR environment variable can be used
    to specify the location directly, for example during development.
    
    Fixes https://github.com/HandBrake/HandBrake/issues/7058

--- gtk/src/application.c
+++ gtk/src/application.c
@@ -58,7 +58,7 @@ struct _GhbApplication
     GtkApplication parent_instance;
     signal_user_data_t *ud;
     GtkBuilder *builder;
-    char *app_cmd;
+    char *app_dir;
     int cancel_encode;
     int when_complete;
     int stderr_src_id;
@@ -673,38 +673,11 @@ video_file_drop_init (signal_user_data_t *ud)
     g_signal_connect(target, "drop", G_CALLBACK(video_file_drop_received), ud);
 }
 
-char *
-ghb_application_get_app_path (GhbApplication *self)
-{
-    g_return_val_if_fail(GHB_IS_APPLICATION(self), NULL);
-
-    // The preferred method, only works on Linux and certain other OSes
-    g_autofree char *link = g_file_read_link("/proc/self/exe", NULL);
-    if (link != NULL)
-        return g_steal_pointer(&link);
-
-    // Alternatively, work out the path from the command name, path and cwd
-    g_autofree char *app_cmd = NULL;
-    g_object_get(self, "app-cmd", &app_cmd, NULL);
-
-    if (g_path_is_absolute(app_cmd))
-        return g_steal_pointer(&app_cmd);
-
-    g_autofree char *path_cmd = g_find_program_in_path(app_cmd);
-    if (path_cmd != NULL)
-        return g_steal_pointer(&path_cmd);
-
-    g_autofree char *cwd = g_get_current_dir();
-    return g_canonicalize_filename(app_cmd, cwd);
-}
-
-char *
+const char *
 ghb_application_get_app_dir (GhbApplication *self)
 {
     g_return_val_if_fail(GHB_IS_APPLICATION(self), NULL);
-
-    g_autofree char *app_path = ghb_application_get_app_path(self);
-    return g_path_get_dirname(app_path);
+    return self->app_dir;
 }
 
 static void
@@ -719,10 +692,9 @@ print_system_information (GhbApplication *self)
     g_printerr("Kernel: %s %s (%s)\n", host_info->sysname,
                host_info->release, host_info->machine);
 #endif
-    g_autofree char *install_dir = ghb_application_get_app_dir(self);
     g_autofree char *config_dir = ghb_get_user_config_dir(NULL);
     g_printerr("CPU: %s x %d\n", hb_get_cpu_name(), hb_get_cpu_count());
-    g_printerr("Install Dir: %s\n", install_dir);
+    g_printerr("Install Dir: %s\n", ghb_application_get_app_dir(self));
     g_printerr("Config Dir:  %s\n", config_dir);
     g_printerr("_______________________________\n\n");
 }
@@ -942,9 +914,9 @@ ghb_application_init (GhbApplication *self)
 }
 
 GhbApplication *
-ghb_application_new (const char *app_cmd)
+ghb_application_new (const char *app_dir)
 {
-    return g_object_new(GHB_TYPE_APPLICATION, "app-cmd", app_cmd, NULL);
+    return g_object_new(GHB_TYPE_APPLICATION, "app-dir", app_dir, NULL);
 }
 
 static void
@@ -1041,7 +1013,7 @@ ghb_application_shutdown (GApplication *app)
 
 enum {
   PROP_0,
-  PROP_APP_CMD,
+  PROP_APP_DIR,
   N_PROPS
 };
 
@@ -1053,8 +1025,8 @@ ghb_application_get_property (GObject *object, guint prop_id,
 
     switch (prop_id)
     {
-        case PROP_APP_CMD:
-            g_value_set_string(value, self->app_cmd);
+        case PROP_APP_DIR:
+            g_value_set_string(value, self->app_dir);
             break;
         default:
             G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
@@ -1069,8 +1041,8 @@ ghb_application_set_property (GObject *object, guint prop_id,
 
     switch (prop_id)
     {
-        case PROP_APP_CMD:
-            self->app_cmd = g_value_dup_string(value);
+        case PROP_APP_DIR:
+            self->app_dir = g_value_dup_string(value);
             break;
         default:
             G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
@@ -1092,12 +1064,11 @@ ghb_application_class_init (GhbApplicationClass *klass)
     object_class->get_property = ghb_application_get_property;
     object_class->set_property = ghb_application_set_property;
 
-    g_autoptr(GParamSpec) prop = g_param_spec_string("app-cmd", "App Command",
-            "The full command-line name of the application", "ghb",
+    g_autoptr(GParamSpec) prop = g_param_spec_string("app-dir", "", "", "ghb",
             G_PARAM_READABLE | G_PARAM_WRITABLE |
             G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS);
 
-    g_object_class_install_property (object_class, PROP_APP_CMD, prop);
+    g_object_class_install_property (object_class, PROP_APP_DIR, prop);
 }
 
 /**
